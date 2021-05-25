8a2b17d1ef0b2cf11e1867f60cf5d7c083911020
Niels Tam Kjer J.R <ntkjer@gmail.com>
2021-05-25 10:37:14 -0400

    Time: 44 ms (46.97%), Space: 14.1 MB (79.80%) - LeetHub

--- next-permutation/next-permutation.py
+++ next-permutation/next-permutation.py
@@ -3,45 +3,33 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        
-        # find first consecutive a[i- 1] < a[i]
-        # find a[j] s.t. it is the min largest val of a[i - 1]
-        # swap a[j] and a[i - 1]
-        # reverse at i onwards
+        # find the first consecutive decreasing pair of nums
+        # find the first value larger than a[i]
+        # swap a[i], a[k]
+        # reverese all items from a[i + 1:len(a)]
         
         i = j = len(nums) - 1
         
         while i > 0 and nums[i - 1] >= nums[i]:
             i -= 1
-            
+           
         if i == 0:
             nums.reverse()
             return
 
-        k = i - 1 
-        while nums[j] <= nums[k]:
+        k = i - 1
+        while j >= 0 and nums[j] <= nums[k]:
             j -= 1
-
+        
         nums[k], nums[j] = nums[j], nums[k]
-        l, r = k + 1, len(nums) - 1
-        while l < r:
-            nums[l], nums[r] = nums[r], nums[l]
-            l, r = l + 1, r - 1
+        
+        r = len(nums) - 1
+        while i < r:
+            nums[i],nums[r] = nums[r], nums[i]
+            i, r = i + 1, r - 1
 
         
-
         
-        
-        
-        
-        
+         
     
-        
             
-
-
-        
-                
-            
-        
-        
