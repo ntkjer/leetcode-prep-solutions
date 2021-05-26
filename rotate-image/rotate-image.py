2c76761f12877d6eaaa9b1579d3e5204dde5f571
Niels Tam Kjer J.R <ntkjer@gmail.com>
2021-05-26 15:15:07 -0400

    Time: 36 ms (57.10%), Space: 14.3 MB (27.85%) - LeetHub

---
+++ rotate-image/rotate-image.py
@@ -0,0 +1,37 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        Two approaches:
+        One hacky and another with two methods that is cleaner and ideal in interview.
+        """
+        m, n = len(matrix), len(matrix[0])
+
+        # this loop will rotate the edges and work inwards
+        # kinda hacky and need to memorize this
+        # for i in range(n // 2 + (n % 2)):
+        #     for j in range(n // 2): 
+        #         tmp = matrix[n - 1 - j][i]
+        #         matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1]
+        #         matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i]
+        #         matrix[j][n - 1 - i] = matrix[i][j]
+        #         matrix[i][j] = tmp
+        self.transpose(matrix)
+        self.reflect(matrix)
+    
+    def transpose(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i, n):
+                matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+    
+    def reflect(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(n // 2):
+                matrix[i][j], matrix[i][-j - 1] = matrix[i][-j - 1], matrix[i][j]
+
+        
+        
+        
+        
