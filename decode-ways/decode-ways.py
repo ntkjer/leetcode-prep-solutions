29263639ef4b393f9ae85524b6195bcd46ea6fc4
Niels Tam Kjer J.R <ntkjer@gmail.com>
2021-06-07 11:22:12 -0400

    Time: 48 ms (9.29%), Space: 14.6 MB (10.59%) - LeetHub

--- decode-ways/decode-ways.py
+++ decode-ways/decode-ways.py
@@ -1,8 +1,8 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
 
-        @lru_cache(maxsize=None)
-        def solve(idx=0) -> int:
+        @lru_cache(None)
+        def solve(idx=0):
             if idx == len(s):
                 return 1
             if s[idx] == "0":
@@ -11,37 +11,14 @@ class Solution:
                 return 1
             
             res = solve(idx + 1)
+            
             pick_two = int(s[idx: idx + 2])
             if pick_two >= 10 and pick_two <= 26:
                 res += solve(idx + 2)
+
             return res
 
-        #res = solve()
-        #solve.cache_clear()
-        
-        # we fill up our dp table to tabulate as we go:
-        #     3  2  6
-        # [0, 1, 1, 2]
-        #        x 
-
-        # we dont add at x mark becase 32 is out of bounds.
-        
-        #     2  2  6
-        # [0, 1, 2, 3]
-
-        # 0 ways to make ""
-        # 1 way to make first char
-        # add the rest until len(s) + 1 (final state)
-
-        dp = [0 for _ in range(len(s) + 1)]
-        dp[0] = 1 # it takes 
-        dp[1] = 1 if s[0] != "0" else 0
-        for i in range(2, len(dp)):
-            if s[i - 1] != "0":
-                dp[i] = dp[i - 1]
-            pick_two = int(s[i - 2: i])
-            if pick_two >= 10 and pick_two <= 26:
-                dp[i] += dp[i - 2]
-
-        res = dp[-1]
+        res = solve()
         return res
+            
+            
