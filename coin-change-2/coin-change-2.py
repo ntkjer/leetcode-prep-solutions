4bc4bd3dd485fb12d0a8c376b2c5de54ea3693a7
Niels Tam Kjer J.R <ntkjer@gmail.com>
2021-06-07 12:05:01 -0400

    Time: 224 ms (52.89%), Space: 14.3 MB (94.73%) - LeetHub

--- coin-change-2/coin-change-2.py
+++ coin-change-2/coin-change-2.py
@@ -1,18 +1,18 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
 
-        @cache #3.9 decorator
-        def solve(amt, idx):
-            if amt == 0:
-                return 1
-
-            if amt < 0 or idx < 0:
-                return 0 
-            
-            return solve(amt - coins[idx], idx) + solve(amt, idx - 1)
-        
-
-        res = solve(amount, len(coins) - 1)
+#         @cache #3.9 decorator
+#         def solve(amt, idx):
+#             if amt == 0:
+#                 return 1
+# 
+#             if amt < 0 or idx < 0:
+#                 return 0 
+#             
+#             return solve(amt - coins[idx], idx) + solve(amt, idx - 1)
+#         
+# 
+        #res = solve(amount, len(coins) - 1)
 
         dp = [0 for _ in range(amount + 1)]
         dp[0] = 1
@@ -20,7 +20,7 @@ class Solution:
             for i in range(coin, amount + 1):
                 dp[i] += dp[i - coin]
 
-        return res
+        return dp[-1] 
 
         
         
