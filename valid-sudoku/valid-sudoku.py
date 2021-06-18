b522fb35a06fc193478a02019a6d8368cd60c38f
Niels Tam Kjer J.R <ntkjer@gmail.com>
2021-06-18 09:29:14 -0700

    Time: 88 ms (94.89%), Space: 14.4 MB (39.49%) - LeetHub

--- valid-sudoku/valid-sudoku.py
+++ valid-sudoku/valid-sudoku.py
@@ -1,28 +1,33 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-    
+
         def is_valid_block(block):
             block = [i for i in block if i != '.']
             return len(block) == len(set(block))
-        
-        def is_valid_row(board):
-            for row in board:
-                if not is_valid_block(row):
+
+        def are_rows_valid(board):
+            for r in board:
+                if not is_valid_block(r):
                     return False
             return True
         
-        def is_valid_col(board):
-            for col in zip(*board):
-                if not is_valid_block(col):
+        def are_cols_valid(board):
+            for c in zip(*board):
+                if not is_valid_block(c):
                     return False
             return True
         
-        def is_valid_square(board):
+        def are_squares_valid(board):
             for i in (0, 3, 6):
                 for j in (0, 3, 6):
-                    square = [board[x][y] for x in range(i, i + 3) for y in range(j, j + 3)]
-                    if not is_valid_block(square):
+                    squares = [board[x][y] for x in range(i, i + 3) for y in range(j, j + 3)]
+                    if not is_valid_block(squares): 
                         return False
             return True
-                    
-        return (is_valid_row(board) and is_valid_col(board) and is_valid_square(board))
+        
+        def valid_sudoku(board):
+            return are_rows_valid(board) and are_cols_valid(board) and are_squares_valid(board)
+            
+        return valid_sudoku(board)
+            
+        
