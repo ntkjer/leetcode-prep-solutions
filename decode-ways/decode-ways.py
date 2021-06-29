d37267f8182f77a4699267e9766a265b92a1af81
Niels Tam Kjer J.R <ntkjer@gmail.com>
2021-06-29 16:05:14 -0400

    Time: 32 ms (68.60%), Space: 14.4 MB (39.06%) - LeetHub

--- decode-ways/decode-ways.py
+++ decode-ways/decode-ways.py
@@ -1,24 +1,36 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
 
-        @lru_cache(None)
+        @cache
         def solve(idx=0):
             if idx == len(s):
                 return 1
-            if s[idx] == "0":
-                return 0
             if idx == len(s) - 1:
                 return 1
+            if s[idx] == "0":
+                return 0
             
-            res = solve(idx + 1)
-            
-            pick_two = int(s[idx: idx + 2])
-            if pick_two >= 10 and pick_two <= 26:
-                res += solve(idx + 2)
+            res = solve(idx + 1) 
+
+            two = int(s[idx: idx + 2])
+            if two >= 10 and two <= 26:
+                res += solve(idx + 2)                
 
             return res
 
-        res = solve()
-        return res
+        dp = [0 for _ in range(len(s) + 1)]
+        dp[0] = 1
+        dp[1] = 1 if s[0] != "0" else 0
+        
+        for i in range(2, len(dp)):
+            if s[i - 1] != "0":
+                dp[i] = dp[i - 1]
+
+            x = int(s[i - 2: i])
+            if 10 <= x <= 26:
+                dp[i] += dp[i - 2]
+
+        return dp[-1]
+        
             
-            
+        
