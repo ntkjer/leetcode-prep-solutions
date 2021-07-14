bab461977ab0f014397bbccd2e25b0665e43d696
Niels Tam Kjer J.R <ntkjer@gmail.com>
2021-07-14 14:40:02 -0400

    Time: 28 ms (95.23%), Space: 14.3 MB (54.26%) - LeetHub

--- alien-dictionary/alien-dictionary.py
+++ alien-dictionary/alien-dictionary.py
@@ -1,52 +1,42 @@
-#class Node(object):
-#    def __init__(self):
-#        self.indegree = 0
-#        self.neighbors = list()
-
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
-        # approach
-        # 1. extract dependency rules from input
-        # 2. put dependency rules into a graph 
-        #       letters as vertex and dependencies as edges
-        # 3. topologically sort the nodes
-
+        
         adj_list = defaultdict(set)
-        in_degree = Counter({c: 0 for word in words for c in word})
-        
-        result = list()    
-        total_edges = 0
-        
+        indegree = Counter({c: 0 for word in words for c in word})
+         
         for first, second in zip(words, words[1:]):
             if len(second) < len(first) and first.startswith(second):
-                return '' # it is a prefix and invalid
+                return ''
             
             for a, b in zip(first, second):
                 if a != b:
                     if b not in adj_list[a]:
-                        in_degree[b] += 1
                         adj_list[a].add(b)
+                        indegree[b] += 1
+                        
                     break
-                    
-        q = collections.deque()         
-        for c in in_degree:
-            if in_degree[c] == 0:
-                q.append(c)
-
-        while q:
-            curr = q.popleft()
-            result.append(curr)
-            for neighbor in adj_list[curr]:
-                in_degree[neighbor] -= 1
+        q = deque()
+        
+        for char in indegree:
+            if indegree[char] == 0:
+                q.append(char)
                 
-                if in_degree[neighbor] == 0:
+        result = list() 
+        
+        while q:
+            curr_course = q.popleft()    
+            result.append(curr_course)     
+            
+            for neighbor in adj_list[curr_course]:
+                indegree[neighbor] -= 1
+                
+                if indegree[neighbor] == 0:
                     q.append(neighbor)
-
-        if len(result) < len(in_degree):
+        
+        if len(result) < len(indegree):
             return ''
         else:
             return ''.join(result)
-
+        
+                    
             
-            
-    
