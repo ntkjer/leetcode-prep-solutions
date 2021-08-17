f46545ecddbd16669d40d9ca9b4e7f7e47355a7a
Niels Tam Kjer J.R <ntkjer@gmail.com>
2021-08-17 07:39:24 -0400

    Time: 136 ms (48.55%), Space: 15.5 MB (48.91%) - LeetHub

--- missing-number/missing-number.py
+++ missing-number/missing-number.py
@@ -1,9 +1,12 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        nums_set = set(nums)   
-        res = None 
-        N = len(nums)
-        nums_inclusive = set([i for i in range(N + 1)])        
-        return set(nums_inclusive - nums_set).pop()
+        #nums_set = set(nums)   
+        #N = len(nums)
+        #nums_inclusive = set([i for i in range(N + 1)])        
+        #return set(nums_inclusive - nums_set).pop()
+        missing = len(nums) 
+        for i, num in enumerate(nums):
+            missing ^= i ^ num
+        return missing
 
             
