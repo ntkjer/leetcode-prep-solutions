e1b554d9dac28296eec0d23c16b61f97baa4327f
Niels Tam Kjer J.R <ntkjer@gmail.com>
2021-10-04 11:02:21 -0400

    Time: 70 ms (5.09%), Space: 14.1 MB (88.92%) - LeetHub

--- game-of-life/game-of-life.py
+++ game-of-life/game-of-life.py
@@ -3,40 +3,48 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        m, n = len(board), len(board[-1])
-        dirs = [(1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (-1, -1), (1, -1), (-1, 1)] 
-        ALIVE = 2 
-        DEAD = -1 
-
-        def count_neighbors(i, j):
-            count = 0
-            return count
+        def valid_dir(x, y):
+            if (x >= 0 and x < M) and (y >= 0 and y < N):
+                return True
+            return False
         
-        def is_alive(i, j):
-            return board[i][j] == 1
-                
-        for i in range(m):
-            for j in range(n):
-                live_neighbors = 0
-                for dx, dy in dirs:
-                    r = dx + i
-                    c = dy + j
-                    if (r < m and r >= 0) and (c < n and c >= 0) and abs(board[r][c]) == 1:
-                        live_neighbors += 1
-
-                if board[i][j] == 1 and (live_neighbors < 2 or live_neighbors > 3):
-                    board[i][j] = DEAD
-                
-                if board[i][j] == 0 and live_neighbors == 3:
-                    board[i][j] = ALIVE
+        directions = [(1, 0), (0, 1), (1, 1), (1, -1), (-1, 1), (0, -1), (-1, -1), (-1, 0)]
+        M, N = len(board), len(board[-1])
+        ALIVE = 2
+        DEAD = -1
         
-        for i in range(m):
-            for j in range(n):
-                if board[i][j] > 0:
-                    board[i][j] = 1
+        for row in range(M):
+            for col in range(N):
+                total_neighbors = 0
+                for dx, dy in directions:
+                    i, j = row + dx, col + dy
+                    if not valid_dir(i, j):
+                        continue
+
+                    if abs(board[i][j]) == 1:
+                        total_neighbors += 1
+
+                if board[row][col] == 1:
+                    if total_neighbors < 2 or total_neighbors > 3:
+                        # under/over populated
+                        board[row][col] = DEAD
+                    # rule 2 is implied
+                elif board[row][col] == 0 and total_neighbors == 3:
+                    board[row][col] = ALIVE
+        
+        for row in range(M):
+            for col in range(N):
+                if board[row][col] >= 1:
+                    board[row][col] = 1
                 else:
-                    board[i][j] = 0
-                    
-                    
-                    
+                    board[row][col] = 0
         
+              
+                    
+                        
+                        
+                    
+                        
+                        
+                
+            
