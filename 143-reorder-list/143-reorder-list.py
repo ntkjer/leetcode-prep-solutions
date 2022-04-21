bb237181cf491252f53efba408343774bedd90bc
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-04-21 23:52:41 +0800

    Time: 82 ms (98.50%), Space: 23.9 MB (68.48%) - LeetHub

--- 143-reorder-list/143-reorder-list.py
+++ 143-reorder-list/143-reorder-list.py
@@ -8,26 +8,24 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        # find halfway point
-        slow = head
-        fast = head.next
+        slow, fast = head, head.next
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-            
+       
         second = slow.next
         prev = slow.next = None
+        
         while second:
             tmp = second.next
             second.next = prev
             prev = second
             second = tmp
-            
-        first = head
-        second = prev
+        
+        first, second = head, prev
+
         while first and second:
             tmp1, tmp2 = first.next, second.next
             first.next = second
             second.next = tmp1
             first, second = tmp1, tmp2
-            
