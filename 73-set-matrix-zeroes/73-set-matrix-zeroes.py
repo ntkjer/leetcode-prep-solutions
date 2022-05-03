a0f9c69d42bafacb92a01ae53950f7140805265b
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-05-04 01:01:25 +0800

    Time: 145 ms (68.51%), Space: 14.9 MB (17.52%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -3,8 +3,9 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        M, N = len(matrix), len(matrix[0])
         rowZero, colZero = False, False
+        M, N = len(matrix), len(matrix[-1])
+        
         for r in range(M):
             for c in range(N):
                 if matrix[r][c] == 0:
@@ -13,19 +14,16 @@ class Solution:
                     if c == 0:
                         colZero = True
                     
-                    matrix[0][c] = matrix[r][0] = 0
-                
-        
+                    matrix[r][0] = matrix[0][c] = 0
         for r in range(1, M):
             for c in range(1, N):
                 if matrix[0][c] == 0 or matrix[r][0] == 0:
                     matrix[r][c] = 0
-        
+                    
         if colZero:
             for r in range(M):
                 matrix[r][0] = 0
-        
+                
         if rowZero:
             for c in range(N):
                 matrix[0][c] = 0
-        
