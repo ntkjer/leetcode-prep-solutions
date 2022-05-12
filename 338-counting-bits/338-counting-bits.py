378c28371986b1098d1d52617a6aa8db76f2c4c5
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-05-12 13:12:42 -0700

    Time: 139 ms (44.22%), Space: 20.8 MB (30.45%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,10 +1,16 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        #     
-        result = [0] * (n + 1)
+        
+        def getOnes(n):
+            res = 0
+            while n:
+                n &= (n - 1)
+                res += 1
+            return res 
+        
+        res = [0] * ((n) + 1)
+        
         for i in range(1, n + 1):
-            result[i] = result[i & (i - 1)] + 1
+            res[i] = 1 + res[i & (i - 1)]
             
-        return result
-            
-            
+        return res
