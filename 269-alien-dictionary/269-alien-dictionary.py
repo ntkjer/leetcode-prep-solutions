f97af7788aeec42eb568a087c86b58d49f8e2b7b
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-05-14 10:47:44 -0700

    Time: 53 ms (34.90%), Space: 14 MB (22.46%) - LeetHub

---
+++ 269-alien-dictionary/269-alien-dictionary.py
@@ -0,0 +1,39 @@
+class Solution:
+    def alienOrder(self, words: List[str]) -> str:
+        adj = {c: set() for w in words for c in w}
+        
+        for i in range(len(words) - 1):
+            w1, w2 = words[i], words[i + 1]
+            
+            minLen = min(len(w1), len(w2))
+            if len(w1) > len(w2) and w1[:minLen] == w2[:minLen]:
+                return ''
+            
+            for j in range(minLen):
+                if w1[j] != w2[j]:
+                    adj[w1[j]].add(w2[j])
+                    break
+        
+        result = list()
+        
+        visit = {}
+        def dfs(c):
+            if c in visit:
+                return visit[c]
+            
+            visit[c] = True
+            for neighbor in adj[c]:
+                if dfs(neighbor):
+                    return True
+        
+            visit[c] = False
+            result.append(c)
+            
+            
+        for c in adj:
+            if dfs(c):
+                return ""
+            
+        result.reverse()
+        return "".join(result)
+                
