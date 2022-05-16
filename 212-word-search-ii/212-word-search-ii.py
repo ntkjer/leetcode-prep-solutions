f1f6278900288909bfddb8e9ca9eb4c4ea4b6b0d
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-05-17 04:01:54 +0800

    Time: 1593 ms (62.92%), Space: 15.5 MB (69.37%) - LeetHub

--- 212-word-search-ii/212-word-search-ii.py
+++ 212-word-search-ii/212-word-search-ii.py
@@ -1,47 +1,48 @@
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        trie = {}    
+        trie = {}
         end = "$"
-        dirs = [(1,0),(0,1),(-1,0),(0,-1)]        
+        
+        for word in words:
+            node = trie
+            for c in word:
+                if c not in node:
+                    node[c] = {}
+                node = node[c]
+            node[end] = True
+
+        M, N = len(board), len(board[0])
         result = list()
         
-
-        M, N = len(board), len(board[-1])
-
-        for w in words:
-            node = trie
-            for ch in w:
-                if ch not in node:
-                    node[ch] = {}
-                node = node[ch]
-            node[end] = True
         
-        
-        def backtrack(r, c, parent, prefix):
+        def dfs(r, c, parent, prefix=""):
+
+            
+            if not(0 <= r < M) or not(0 <= c < N):
+                return
+            
             letter = board[r][c]
             if letter in parent:
                 board[r][c] = "#"
+                if letter not in parent:
+                    return 
                 node = parent[letter]
                 match = node.pop(end, False)
                 if match:
                     result.append(prefix + letter)
-                    #del node[end]
-                    
-                for dx, dy in dirs:
-                    x, y = r + dx, c + dy
-                    if 0 <= x < M and 0 <= y < N and board[x][y] != "#" and board[x][y] in node:
-                        backtrack(x, y, node, prefix + letter)
-                        
+
+                for dx, dy in [(1, 0), (-1,0), (0, 1), (0, -1)]:
+                    x, y = dx + r, dy + c
+                    if (0 <= x < M and 0 <= y < N):
+                        if board[x][y] in node:
+                            dfs(x, y, node, prefix + letter)
+
                 board[r][c] = letter
-                #optimize by removing matched leaf nodes from trie
                 if not node:
                     parent.pop(letter)
         
         for r in range(M):
             for c in range(N):
-                backtrack(r, c, trie, "")
-
-        return result
-            
+                dfs(r, c, trie)
                 
-            
+        return result
