5753f7dbbbf54d35177e6afa606b1841cb86466e
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-05-17 02:04:36 +0800

    Time: 2229 ms (51.19%), Space: 15.6 MB (51.58%) - LeetHub

--- 212-word-search-ii/212-word-search-ii.py
+++ 212-word-search-ii/212-word-search-ii.py
@@ -1,49 +1,46 @@
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        M, N = len(board), len(board[0])
-        trie = {}
-        end = "#"
+        trie = {}    
+        end = "$"
+        dirs = [(1,0),(0,1),(-1,0),(0,-1)]        
+        result = list()
+        visited = set()
+
+        M, N = len(board), len(board[-1])
+
         for w in words:
             node = trie
-            for c in w:
-                if c not in node:
-                    node[c] = {}
-                node = node[c]
+            for ch in w:
+                if ch not in node:
+                    node[ch] = {}
+                node = node[ch]
             node[end] = True
         
-        visit, res = set(), list()
-        directions = [(1,0), (-1, 0), (0, 1), (0, -1)]
         
-        def validPos(x,y):
-            if not(0 <= x < M) or not(0 <= y < N):
-                return False
-            return True
-        
-        def dfs(r, c, parent, word=""):
-            if not validPos(r,c):
-                return  
+        def backtrack(r, c, parent, prefix):
             letter = board[r][c]
-            if letter not in parent or (r,c) in visit:
-                return
-            
-            visit.add((r,c))
-            
-            node = parent[letter]
-            if end in node:
-                res.append(word + letter)
-                del node[end] # mark for deletion
-            for dx, dy in directions:
-                x, y = dx + r, dy + c
-                if validPos(x,y) and (x,y) not in visit:
-                    dfs(x, y, node, word + letter)
+            if letter in parent:
+                visited.add((r, c))    
+                node = parent[letter]
+                if end in node:
+                    result.append(prefix + letter)
+                    del node[end]
                     
-            visit.remove((r,c))
-            if not node:
-                parent.pop(letter)
-        
+                for dx, dy in dirs:
+                    x, y = r + dx, c + dy
+                    if 0 <= x < M and 0 <= y < N and (x, y) not in visited:
+                        backtrack(x, y, node, prefix + letter)
+                visited.remove((r, c))
+
+                #optimize by removing matched leaf nodes from trie
+                if not node:
+                    parent.pop(letter)
         
         for r in range(M):
             for c in range(N):
-                dfs(r, c, trie)
-        
-        return res
+                backtrack(r, c, trie, "")
+
+        return result
+            
+                
+            
