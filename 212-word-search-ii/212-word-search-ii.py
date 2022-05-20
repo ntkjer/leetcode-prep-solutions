77699d5531ec51fd8b12f6211e7fa8ffc3c7d272
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-05-20 23:42:16 +0800

    Time: 1676 ms (60.58%), Space: 15.5 MB (91.01%) - LeetHub

--- 212-word-search-ii/212-word-search-ii.py
+++ 212-word-search-ii/212-word-search-ii.py
@@ -1,5 +1,6 @@
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        
         trie = {}
         end = "$"
         
@@ -10,39 +11,34 @@ class Solution:
                     node[c] = {}
                 node = node[c]
             node[end] = True
-
-        M, N = len(board), len(board[0])
+        
+        visit = set()
+        ROWS, COLS = len(board), len(board[0])
         result = list()
         
-        
         def dfs(r, c, parent, prefix=""):
-
-            
-            if not(0 <= r < M) or not(0 <= c < N):
-                return
-            
+            if ((r,c) in visit or not(0 <= r < ROWS) 
+                or not(0 <= c < COLS) or board[r][c] not in parent):
+                return False
+            visit.add((r,c))
             letter = board[r][c]
-            if letter in parent:
-                board[r][c] = "#"
-                if letter not in parent:
-                    return 
-                node = parent[letter]
-                match = node.pop(end, False)
-                if match:
-                    result.append(prefix + letter)
-
-                for dx, dy in [(1, 0), (-1,0), (0, 1), (0, -1)]:
-                    x, y = dx + r, dy + c
-                    if (0 <= x < M and 0 <= y < N):
-                        if board[x][y] in node:
-                            dfs(x, y, node, prefix + letter)
-
-                board[r][c] = letter
-                if not node:
-                    parent.pop(letter)
-        
-        for r in range(M):
-            for c in range(N):
-                dfs(r, c, trie)
+            node = parent[letter]
+            match = node.pop(end, False)
+            if match:
+                result.append(prefix + letter)
+            
+            dfs(r + 1, c, node, prefix + letter)
+            dfs(r - 1, c, node, prefix + letter)
+            dfs(r, c + 1, node, prefix + letter)
+            dfs(r, c - 1, node, prefix + letter)
+            
+            visit.remove((r,c))
+            
+            if not node:
+                parent.pop(letter)
                 
+        for r in range(ROWS):
+            for c in range(COLS):
+                dfs(r, c, trie, "")
+        
         return result
