5fe6fda020e1096775fe63e8c30c7172617012df
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-05-25 23:13:35 +0800

    Time: 64 ms (14.81%), Space: 14.2 MB (8.38%) - LeetHub

--- 269-alien-dictionary/269-alien-dictionary.py
+++ 269-alien-dictionary/269-alien-dictionary.py
@@ -1,18 +1,20 @@
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
-        adj = {c: set() for word in words for c in word }
+        adj = {c : set() for word in words for c in word}
         
         for i in range(len(words) - 1):
             w1, w2 = words[i], words[i + 1]
-            length = min(len(w1), len(w2))
             
-            if len(w1) > len(w2) and w1[:length] == w2[:length]:
+            minLength = min(map(len, (w1, w2)))
+            if len(w1) > len(w2) and w1[:minLength] == w2[:minLength]:
                 return ""
             
-            for j in range(length):
+            for j in range(minLength):
                 if w1[j] != w2[j]:
                     adj[w1[j]].add(w2[j])
                     break
+
+        
         visit = {}
         result = list()
         
@@ -21,18 +23,17 @@ class Solution:
                 return visit[c]
             
             visit[c] = True
-            
             for nei in adj[c]:
                 if dfs(nei):
                     return True
-                
             visit[c] = False
             result.append(c)
-        
+            
+            
         for c in adj:
             if dfs(c):
                 return ""
         
         result.reverse()
-        
         return "".join(result)
+            
