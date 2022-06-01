e48b02df451eba0c7bedc76b2842049f5ccb54bb
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-06-02 01:09:27 +0800

    Time: 60 ms (18.00%), Space: 14.1 MB (28.56%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -22,5 +22,18 @@ class Solution:
                 
             return res
         
+        if s[0] == "0" or len(s) == 0:
+            return 0
         
-        return backtrack()
+        dp = [0] * (len(s) + 1)
+        dp[0] = 1 
+        dp[1] = 1 if s[0] != "0" else 0
+        for i in range(2, len(dp)):
+            if int(s[i - 1]) != 0:
+                dp[i] += dp[i - 1]
+            take_two = int(s[i - 2: i])
+            if 10 <= take_two <= 26:
+                dp[i] += dp[i - 2]
+            
+                
+        return dp[-1]
