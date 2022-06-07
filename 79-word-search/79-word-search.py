14de3241d5690e6a4b59fffb49bc1153e88e155d
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-06-07 18:53:43 +0800

    Time: 9203 ms (10.51%), Space: 13.9 MB (92.70%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,34 +1,25 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        
+        visit = set()
         ROWS, COLS = len(board), len(board[-1])
         
-        visit = set()
-        
-        def dfs(r, c, pos):
-            if pos == len(word):
+        def dfs(r, c, idx=0):
+            if idx == len(word):
                 return True
-            
-            if ((r,c) in visit or 
-                not (0 <= r < ROWS) or 
-                not (0 <= c < COLS) or 
-                board[r][c] != word[pos]):
+            if (r,c) in visit or not 0 <= r < ROWS or not 0 <= c < COLS or word[idx] != board[r][c]:
                 return False
-            
+  
             visit.add((r,c))
-            res = (dfs(r + 1, c, pos + 1) or 
-                   dfs(r, c + 1, pos + 1) or 
-                   dfs(r - 1, c, pos + 1) or 
-                   dfs(r, c - 1, pos + 1))
+            res = (dfs(r + 1, c, idx + 1) or dfs(r, c + 1, idx + 1)
+                   or dfs(r - 1, c, idx + 1) or dfs(r, c - 1, idx + 1))
             visit.remove((r,c))
             return res
-        
+            
         for r in range(ROWS):
             for c in range(COLS):
                 start_idx = 0
                 if board[r][c] == word[start_idx]:
-                    if dfs(r,c, start_idx):
+                    if dfs(r, c, start_idx):
                         return True
-                    
-                    
         return False
+        
