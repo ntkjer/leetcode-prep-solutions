273cb1a509d9adcb71bf542cb5d7b762d851edb1
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-06-17 12:22:02 -0400

    Time: 212 ms (43.98%), Space: 14.1 MB (19.22%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -1,16 +1,17 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        l = 0
         counts = {}
         res = 0
-        l = 0
         maxFreq = 0
+        
         for r in range(len(s)):
-            counts[s[r]] = 1 + counts.get(s[r], 0)
+            counts[s[r]] = counts.get(s[r], 0) + 1
+            
             maxFreq = max(maxFreq, counts[s[r]])
             
             while (r - l + 1) - maxFreq > k:
                 counts[s[l]] -= 1
                 l += 1
-                
             res = max(res, r - l + 1)
         return res
