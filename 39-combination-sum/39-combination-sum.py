72ca3b46181db3f3ae85b575643053436645a676
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-06-22 16:38:45 -0400

    Time: 111 ms (59.17%), Space: 14.1 MB (42.42%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,20 +1,20 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        res = list()
         
-        def solve(idx=0, partial=[], curr=0):
+        res = []
+        
+        def solve(idx, curr, partial):
             if curr == target:
                 res.append(partial[:])
                 return
-            
             if idx >= len(candidates) or curr > target:
                 return
             
             partial.append(candidates[idx])
-            solve(idx, partial, curr + candidates[idx])
+            solve(idx, curr + candidates[idx], partial)
             partial.pop()
-            solve(idx + 1, partial, curr)
+            solve(idx + 1, curr, partial)
             return
         
-        solve()
+        solve(0, 0, [])
         return res
