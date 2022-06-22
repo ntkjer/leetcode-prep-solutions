15f1df78f5b0414db9fd303051e250fb5530b0e2
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-06-22 13:11:38 -0400

    Time: 7882 ms (39.20%), Space: 13.9 MB (50.61%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -3,23 +3,27 @@ class Solution:
         visit = set()
         ROWS, COLS = len(board), len(board[-1])
         
-        def dfs(r, c, idx=0):
+        def backtrack(r, c, idx):
             if idx == len(word):
                 return True
-            if (r,c) in visit or not 0 <= r < ROWS or not 0 <= c < COLS or word[idx] != board[r][c]:
+            
+            if (r,c) in visit or not(0 <= r < ROWS) or not(0 <= c < COLS) or board[r][c] != word[idx]:
                 return False
-  
-            visit.add((r,c))
-            res = (dfs(r + 1, c, idx + 1) or dfs(r, c + 1, idx + 1)
-                   or dfs(r - 1, c, idx + 1) or dfs(r, c - 1, idx + 1))
+            
+            visit.add((r,c))    
+            res = (backtrack(r + 1, c, idx + 1) or backtrack(r - 1, c, idx + 1) or 
+                   backtrack(r, c + 1, idx + 1) or backtrack(r, c - 1, idx + 1))
+            
             visit.remove((r,c))
             return res
-            
+        
+        
         for r in range(ROWS):
             for c in range(COLS):
-                start_idx = 0
-                if board[r][c] == word[start_idx]:
-                    if dfs(r, c, start_idx):
+                if word[0] == board[r][c]:
+                    if backtrack(r, c, 0):
                         return True
         return False
+            
+        
         
