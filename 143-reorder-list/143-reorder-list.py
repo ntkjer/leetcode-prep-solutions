bb18dc956a68e66d7967d76637c2b1b4bf18e12f
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-06-27 13:45:27 -0400

    Time: 128 ms (53.08%), Space: 23.9 MB (95.53%) - LeetHub

--- 143-reorder-list/143-reorder-list.py
+++ 143-reorder-list/143-reorder-list.py
@@ -8,12 +8,14 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        slow, fast = head, head.next
+        # find the ending node and halfway point
+        slow = head
+        fast = head.next
         while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-       
+            slow, fast = slow.next, fast.next.next
+        
         second = slow.next
+        
         prev = slow.next = None
         
         while second:
@@ -23,9 +25,15 @@ class Solution:
             second = tmp
         
         first, second = head, prev
-
+        
         while first and second:
             tmp1, tmp2 = first.next, second.next
             first.next = second
             second.next = tmp1
+            
             first, second = tmp1, tmp2
+     
+            
+        
+        
+        
