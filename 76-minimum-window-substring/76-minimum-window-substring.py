8b902f50e204b2950c082ec11115769436ac523f
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-06-27 17:01:45 -0400

    Time: 163 ms (51.56%), Space: 14.7 MB (82.55%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -1,30 +1,29 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        
         window = {}
-        target = Counter(t)
+        target = collections.Counter(t)
+        
         l = 0
-        res = float('inf'), 0, 0
         found, desired = 0, len(target)
+        res = float('inf'), 0, 0
         
         for r in range(len(s)):
-            char = s[r]
-            window[char] = window.get(char, 0) + 1
+            curr = s[r]
+            window[curr] = window.get(curr, 0) + 1
             
-            if char in target and window[char] == target[char]:
+            if window[curr] == target[curr]:
                 found += 1
-
-            while l <= r and found == desired:
-
+            
+            while found == desired and l <= r:
                 if (r - l + 1) < res[0]:
                     res = (r - l + 1), l, r
                     
                 start = s[l]
                 window[start] -= 1
                 
-                if start in target and window[start] < target[start]:
+                if window[start] < target[start]:
                     found -= 1
-                    
+                
                 l += 1
-    
-        return "" if res[0] == float('inf') else s[res[1]: res[2] + 1]                
+                
+        return "" if res[0] == float('inf') else s[res[1]: res[2] + 1]
