d6f48a0634406e3943aa039364d9d303ebad003d
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-06-30 09:29:09 -0400

    Time: 3041 ms (45.68%), Space: 15.6 MB (70.59%) - LeetHub

--- 212-word-search-ii/212-word-search-ii.py
+++ 212-word-search-ii/212-word-search-ii.py
@@ -1,51 +1,48 @@
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        ROWS, COLS = len(board), len(board[-1])
+        res = list()
+        rows, cols = len(board), len(board[-1])
         visit = set()
         
         trie = {}
         end = "$"
         
         for word in words:
-            node = trie
+            root = trie
             for ch in word:
-                if ch not in node:
-                    node[ch] = {}
-                node = node[ch]
-            node[end] = True
+                if ch not in root:
+                    root[ch] = {}
+                root = root[ch]
+            root[end] = True
         
-        res = list()
-        
-        def dfs(r, c, parent, word):
-            if (r,c) in visit or not(0 <= r < ROWS) or not(0 <= c < COLS):
-                return False
+        def dfs(r, c, word, parent):
+            if (r, c) in visit or not(0 <= r < rows) or not(0 <= c < cols):
+                return 
+            letter = board[r][c]
+            if letter not in parent:
+                return
             
-            curr = board[r][c]
-            if curr not in parent:
-                return False
-            
-            node = parent[curr]
-            
-            visit.add((r,c))
-            
+            node = parent[letter]
+            visit.add((r, c))
             match = node.pop(end, False)
+            
             if match:
-                res.append(word+curr)
+                res.append(word + letter)
                 
-            dfs(r + 1, c, node, word+curr)
-            dfs(r, c + 1, node, word+curr)
-            dfs(r - 1, c, node, word+curr)
-            dfs(r, c - 1, node, word+curr)
+                
+            dfs(r + 1, c, word + letter, node)
+            dfs(r, c + 1, word + letter, node)
+            dfs(r, c - 1, word + letter, node)
+            dfs(r - 1, c, word + letter, node)
             
             visit.remove((r,c))
             if not node:
-                parent.pop(curr)
-                
+                parent.pop(letter)
+            
         
-        
-        for r in range(ROWS):
-            for c in range(COLS):
+        for r in range(rows):
+            for c in range(cols):
                 root = trie
-                dfs(r, c, root, "")
-        
+                dfs(r, c, "", root)
+                
         return res
