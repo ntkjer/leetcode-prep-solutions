414c6477ef265b4b97909219263a2af1ff72fabc
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-07-05 16:36:09 -0400

    Time: 143 ms (19.99%), Space: 14.1 MB (86.93%) - LeetHub

--- 271-encode-and-decode-strings/271-encode-and-decode-strings.py
+++ 271-encode-and-decode-strings/271-encode-and-decode-strings.py
@@ -1,30 +1,32 @@
 class Codec:
+    def __init__(self):
+        self.delimeter = "$"
+        
     def encode(self, strs: List[str]) -> str:
         """Encodes a list of strings to a single string.
         """
         res = list()
-        
         for word in strs:
-            res.append(str(len(word)) + "$" + word)
-        
-        return ",".join(res)
+            res.append(str(len(word)) + self.delimeter + word)
+        return "".join(res)
 
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
+        res = list()
         i = 0
         j = 0
-        N = len(s)
-        res = list()
-        while i < N:
-            while s[i] != "$":
+        while i < len(s):
+            while s[i] != self.delimeter:
                 i += 1
             length = int(s[j: i])
-            res.append(s[i + 1: length + i + 1])
-            i = length + i + 1
-            j = i + 1
             
+            res.append(s[i + 1: i + length + 1])
+            
+            i = length + 1 + i
+            j = i
         return res
+        
         
 
 
