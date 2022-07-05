8df505aa7b6f3a6745fa87f75d9be15ab8fe0170
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-07-05 12:02:17 -0400

    Time: 70 ms (7.82%), Space: 13.8 MB (73.69%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -5,6 +5,6 @@ class Solution:
         
         for i in range(1, n):
             for j in range(1, m):
-                dp[i][j] = dp[i][j - 1] + dp[i - 1][j]
-                
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        
         return dp[-1][-1]
