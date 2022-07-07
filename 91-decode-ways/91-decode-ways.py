ea973364ae24a748c927cc89fad374f26d923d3f
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-07-07 07:31:55 -0700

    Time: 51 ms (46.73%), Space: 13.9 MB (45.01%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,17 +1,22 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         
-        dp = [0] * (len(s) + 1)
+        # s = "12", 12 and 1,2
+        #           L      A,B
         
+        dp = [0 for _ in range(len(s) + 1)]
         dp[0] = 1
+        
         dp[1] = 1 if s[0] != "0" else 0
         
         for i in range(2, len(dp)):
-            pick_two = int(s[i - 2: i])
-            if 1 <= int(s[i - 1]) <= 26:
+            pick_one = s[i - 1]
+            if 1 <= int(pick_one) <= 26:
                 dp[i] += dp[i - 1]
-            
+                
+            pick_two = int(s[i - 2: i])
             if 10 <= pick_two <= 26:
                 dp[i] += dp[i - 2]
-      
+            
         return dp[-1]
+                
