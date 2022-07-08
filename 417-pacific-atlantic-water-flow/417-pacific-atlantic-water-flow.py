1c84b2ff218c02d6073ee724ed3f459acaa2b3a5
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-07-08 09:54:12 -0700

    Time: 551 ms (25.35%), Space: 16 MB (16.50%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -1,31 +1,26 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        rows, cols = len(heights), len(heights[-1])
         pac, atl = set(), set()
-        ROWS, COLS = len(heights), len(heights[-1])
         
-        def dfs(r, c, prev, visit):
-            if (r,c) in visit or not (0 <= r < ROWS) or not(0 <= c < COLS) or prev > heights[r][c]:
-                return False
-            
+        def dfs(r, c, visit, prev):
+            if ((r,c) in visit or not(0 <= r < rows) or 
+                not(0 <= c < cols) or prev > heights[r][c]):
+                return
             visit.add((r,c))
-            curr = heights[r][c]    
-            dfs(r + 1, c, curr, visit)
-            dfs(r - 1, c, curr, visit)
-            dfs(r, c + 1, curr, visit)
-            dfs(r, c - 1, curr, visit)
             
-            return True
+            dfs(r + 1, c, visit, heights[r][c])
+            dfs(r, c + 1, visit, heights[r][c])
+            dfs(r, c - 1, visit, heights[r][c])
+            dfs(r - 1, c, visit, heights[r][c])
+            return
         
+        for r in range(rows):
+            dfs(r, 0, atl, heights[r][0])
+            dfs(r, cols - 1, pac, heights[r][cols - 1])
         
-        for r in range(ROWS):
-            dfs(r, COLS - 1, heights[r][COLS - 1], pac)
-            dfs(r, 0, heights[r][0], atl)
-        
-        for c in range(COLS):
-            dfs(0, c, heights[0][c], atl)
-            dfs(ROWS - 1, c, heights[ROWS - 1][c], pac)
-        
+        for c in range(cols):
+            dfs(0, c, atl, heights[0][c])
+            dfs(rows - 1, c, pac, heights[rows - 1][c])
+            
         return pac & atl
-        
-        
-        
