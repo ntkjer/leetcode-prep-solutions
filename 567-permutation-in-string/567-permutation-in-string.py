620c06b2c1361136cb2fbc50e45fe79ccdc21303
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-07-11 13:25:33 -0700

    Time: 96 ms (75.76%), Space: 13.9 MB (95.92%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,34 +1,39 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2): return False
+        if len(s1) > len(s2): 
+            return False
         
         s1Count, s2Count = [0] * 26, [0] * 26
+        matches = 0
+        
         for i in range(len(s1)):
             s1Count[ord(s1[i]) - ord('a')] += 1
             s2Count[ord(s2[i]) - ord('a')] += 1
-        match = 0
-        for i in range(26):
-            match += (1 if s1Count[i] == s2Count[i] else 0)
-        l = 0
-        for r in range(len(s1), len(s2)):
-            if match == 26:
-                return True            
-            # right 
-            index = ord(s2[r]) - ord('a')
-            s2Count[index] += 1
-            if s1Count[index] == s2Count[index]:
-                match += 1
-            elif s1Count[index] + 1 == s2Count[index]:
-                match -= 1
-            
-            
-            index = ord(s2[l]) - ord('a')
-            s2Count[index] -= 1
-            if s1Count[index] == s2Count[index]:
-                match += 1
+        
+        for i in range(len(s1Count)):
+            if s1Count[i] == s2Count[i]:
+                matches += 1
                 
-            elif s1Count[index] - 1 == s2Count[index]:
-                match -= 1
+        l = 0
+        
+        for r in range(len(s1), len(s2)):
+            if matches == 26:
+                return True
+            
+            idx = ord(s2[r]) - ord('a')
+            s2Count[idx] += 1
+            if s2Count[idx] == s1Count[idx]:
+                matches += 1
+            elif s2Count[idx] == s1Count[idx] + 1:
+                matches -= 1
+            
+            idx = ord(s2[l]) - ord('a')
+            s2Count[idx] -= 1
+            if s2Count[idx] == s1Count[idx]:
+                matches += 1
+            elif s2Count[idx] == s1Count[idx] - 1:
+                matches -= 1
                 
             l += 1
-        return match == 26
+            
+        return matches == 26
