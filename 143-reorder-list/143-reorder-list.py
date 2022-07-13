d9aba22935e54e507c4d59f70e32994659072509
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-07-13 13:26:03 -0700

    Time: 184 ms (12.16%), Space: 23.9 MB (95.56%) - LeetHub

--- 143-reorder-list/143-reorder-list.py
+++ 143-reorder-list/143-reorder-list.py
@@ -8,25 +8,22 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        # split list in half using 2 pointers 
-        # reverse second half
-        # merge first and second
-        dummy = ListNode(next=head)
-        
+        dummy = ListNode(next = head)
         slow, fast = head, head.next
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
         
         second = slow.next
-        slow.next = None
         prev = None
         while second:
             tmp = second.next
             second.next = prev
             prev = second
             second = tmp
-        first = dummy.next
+       
+        slow.next = None
+        first = head
         second = prev
         
         while first and second:
@@ -34,4 +31,5 @@ class Solution:
             first.next = second
             second.next = tmp1
             first, second = tmp1, tmp2
-            
+        
+        
