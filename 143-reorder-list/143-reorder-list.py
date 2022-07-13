ce745da9030332a4e46cef63eb37ffe90b699ea0
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-07-13 13:26:28 -0700

    Time: 96 ms (89.40%), Space: 23.9 MB (95.56%) - LeetHub

--- 143-reorder-list/143-reorder-list.py
+++ 143-reorder-list/143-reorder-list.py
@@ -9,7 +9,7 @@ class Solution:
         Do not return anything, modify head in-place instead.
         """
         dummy = ListNode(next = head)
-        slow, fast = head, head.next
+        slow, fast = head, head
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
