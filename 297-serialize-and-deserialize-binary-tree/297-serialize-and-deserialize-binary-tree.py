85065d3cca8bdbe60c8ba584bf3b8757e99c058c
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-07-13 13:47:56 -0700

    Time: 120 ms (95.28%), Space: 20.6 MB (21.36%) - LeetHub

--- 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.py
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.py
@@ -14,7 +14,6 @@ class Codec:
         :rtype: str
         """
         res = list()
-        
         def solve(node):
             if not node:
                 res.append("None")
@@ -22,9 +21,7 @@ class Codec:
             res.append(str(node.val))
             solve(node.left)
             solve(node.right)
-            return
-        
-        solve(root)
+        solve(root)   
         return ",".join(res)
         
 
@@ -34,23 +31,21 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
-        self.idx = 0
         data = data.split(",")
+ 
+        self.idx = 0
         def solve():
-            if data[self.idx] == "None":
+            if data[self.idx] == "None" or self.idx >= len(data):
                 self.idx += 1
-                return
+                return None 
             
             curr = TreeNode(data[self.idx])
             self.idx += 1
             curr.left = solve()
             curr.right = solve()
             return curr
-        
         root = solve()
         return root
-            
-        
 
 # Your Codec object will be instantiated and called as such:
 # ser = Codec()
