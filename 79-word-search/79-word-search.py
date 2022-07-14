1f3be928fcc6eb9f93c71cd5feecb7ce0836bff7
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-07-14 06:44:02 -0700

    Time: 5408 ms (81.72%), Space: 14 MB (12.30%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,31 +1,34 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         visit = set()
-        rows, cols = len(board), len(board[0])
+        rows, cols = len(board), len(board[-1])
         
-        def solve(r, c, idx):
+        
+ 
+        def backtrack(r, c, idx):
             if idx == len(word):
                 return True
-            if (r, c) in visit or not (0 <= r < rows) or not (0 <= c < cols):
+            
+            if (r, c) in visit:
                 return False
+            
+            if not (0 <= r < rows) or not (0 <= c < cols):
+                return False
+            
             letter = board[r][c]
             if letter != word[idx]:
                 return False
-            visit.add((r,c))
-            res = (solve(r + 1, c, idx + 1) or solve(r, c + 1, idx + 1) or
-                  solve(r - 1, c, idx + 1) or solve(r, c - 1, idx + 1))
-                   
+            visit.add((r, c))
+           
+            
+            res = (backtrack(r + 1, c, idx + 1) or backtrack(r, c + 1, idx + 1) or
+                   backtrack(r - 1, c, idx + 1) or backtrack(r, c - 1, idx + 1))
             visit.remove((r,c))
-            return res
-
+            return res 
         
         for r in range(rows):
             for c in range(cols):
-                if board[r][c] == word[0]:
-                    if solve(r, c, 0):
+                if word[0] == board[r][c]:
+                    if backtrack(r, c, 0):
                         return True
-                    
         return False
-                
-        
-                  
