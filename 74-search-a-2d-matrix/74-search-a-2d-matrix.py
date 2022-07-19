d5f1ee9e291e3378fd489f3856f21b55c4fe1a02
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-07-18 17:14:04 -0700

    Time: 59 ms (68.03%), Space: 14.4 MB (42.69%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,30 +1,30 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        # first integer of reach row is gt than last integer of previous row
         rows, cols = len(matrix), len(matrix[-1])
         
+        # binary search to find the correct row 
         top, bottom = 0, rows - 1
-        # isolate row
         while top <= bottom:
-            row = (top + bottom) // 2
-            if target > matrix[row][-1]:
-                top = row + 1
-            elif target < matrix[row][0]:
-                bottom = row - 1
+            curr_row = (top + bottom) // 2
+            if target > matrix[curr_row][-1]:
+                top = curr_row + 1
+            elif target < matrix[curr_row][0]:
+                bottom = curr_row - 1
             else:
                 break
-        if top > bottom:
-            return False
+        if top > bottom: return False
         
+        # binary search finding the value itself amongst the correct column
         row = (top + bottom) // 2
+        
         l, r = 0, cols - 1
         while l <= r:
             mid = (l + r) // 2
-            if target > matrix[row][mid]:
-                l = mid + 1
-            elif target < matrix[row][mid]:
+            if matrix[row][mid] == target:
+                return True
+            elif matrix[row][mid] > target:
                 r = mid - 1
             else:
-                return True
+                l = mid + 1
             
         return False
