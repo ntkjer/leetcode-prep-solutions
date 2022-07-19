372c91c0254b73dc51aecdd1de9af3c7b2cd5070
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-07-19 11:43:57 -0700

    Time: 89 ms (15.31%), Space: 13.9 MB (68.66%) - LeetHub

--- 1274-number-of-ships-in-a-rectangle/1274-number-of-ships-in-a-rectangle.py
+++ 1274-number-of-ships-in-a-rectangle/1274-number-of-ships-in-a-rectangle.py
@@ -12,20 +12,19 @@
 
 class Solution:
     def countShips(self, sea: 'Sea', topRight: 'Point', bottomLeft: 'Point') -> int:
-        # what is the slope, and can we binary search between topRight point to bottomLeft and collect answers?
-            # find xRange and yRange? 
-        if bottomLeft.x > topRight.x or bottomLeft.y > topRight.y:
+        
+        if topRight.x < bottomLeft.x or topRight.y < bottomLeft.y:
             return 0
         if not sea.hasShips(topRight, bottomLeft):
             return 0
-        
         if topRight.x == bottomLeft.x and topRight.y == bottomLeft.y:
             return 1
         
         midX = (topRight.x + bottomLeft.x) // 2
         midY = (topRight.y + bottomLeft.y) // 2
-        res = self.countShips(sea, Point(midX, midY), bottomLeft) + \
-               self.countShips(sea, topRight, Point(midX + 1, midY + 1)) + \
-               self.countShips(sea, Point(midX, topRight.y), Point(bottomLeft.x, midY + 1)) + \
-               self.countShips(sea, Point(topRight.x, midY), Point(midX + 1, bottomLeft.y))
-        return res
+        
+        rect1 = self.countShips(sea, Point(midX, midY), bottomLeft)
+        rect2 = self.countShips(sea, Point(topRight.x, midY), Point(midX + 1, bottomLeft.y))
+        rect3 = self.countShips(sea, topRight, Point(midX + 1, midY + 1))
+        rect4 = self.countShips(sea, Point(midX, topRight.y), Point(bottomLeft.x, midY + 1))
+        return rect1 + rect2 + rect3 + rect4
