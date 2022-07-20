af2bfb7feeb6bf8add34c229dbec538a21b80106
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-07-19 17:43:52 -0700

    Time: 143 ms (63.63%), Space: 19.6 MB (29.96%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,12 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
-        groups = {}
-        for word in strs:
-            freqs = [0] * 26
-            for ch in word:
-                freqs[ord(ch) - ord('a')] += 1
-                
-            groups[tuple(freqs)] = groups.get(tuple(freqs), []) + [word]
+        chars = [0] * 26
+        anagrams = {}
         
-        return groups.values()
+        for word in strs:
+            chars = [0] * 26
+            for ch in word:
+                chars[ord(ch) - ord('a')] += 1
+            anagrams[tuple(chars)] = anagrams.get(tuple(chars), []) + [word]
+        
+        return anagrams.values()
