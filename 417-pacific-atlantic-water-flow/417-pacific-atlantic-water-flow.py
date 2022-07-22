3d4dbf89c64e24b4b22f59d6302e6649e1c0cd06
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-07-22 08:06:12 -0700

    Time: 586 ms (21.46%), Space: 15.7 MB (55.86%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -1,26 +1,34 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        rows, cols = len(heights), len(heights[-1])
-        pac, atl = set(), set()
+        rows = len(heights)
+        cols = len(heights[-1])
         
-        def dfs(r, c, visit, prev):
-            if ((r,c) in visit or not(0 <= r < rows) or 
-                not(0 <= c < cols) or prev > heights[r][c]):
+        pac = set()
+        atl = set()
+        
+        def dfs(r, c, prev, visit):
+            if (r,c) in visit: 
                 return
-            visit.add((r,c))
             
-            dfs(r + 1, c, visit, heights[r][c])
-            dfs(r, c + 1, visit, heights[r][c])
-            dfs(r, c - 1, visit, heights[r][c])
-            dfs(r - 1, c, visit, heights[r][c])
-            return
-        
+            if not(0 <= r < rows) or not(0 <= c < cols):
+                return 
+            
+            if heights[r][c] < prev:
+                return
+            
+            visit.add((r,c))
+            curr = heights[r][c]
+            dfs(r + 1, c, curr, visit)
+            dfs(r, c + 1, curr, visit)
+            dfs(r, c - 1, curr, visit)
+            dfs(r - 1, c, curr, visit)
+            
         for r in range(rows):
-            dfs(r, 0, atl, heights[r][0])
-            dfs(r, cols - 1, pac, heights[r][cols - 1])
+            dfs(r, 0, heights[r][0], atl)
+            dfs(r, cols - 1, heights[r][cols - 1], pac)
         
         for c in range(cols):
-            dfs(0, c, atl, heights[0][c])
-            dfs(rows - 1, c, pac, heights[rows - 1][c])
+            dfs(0, c, heights[0][c], atl)
+            dfs(rows - 1, c, heights[rows - 1][c], pac)
             
         return pac & atl
