33ec432cc8c5affb0b6f098f1ca52c98c332a20c
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-07-22 10:33:47 -0700

    Time: 78 ms (6.57%), Space: 14.2 MB (9.23%) - LeetHub

--- 269-alien-dictionary/269-alien-dictionary.py
+++ 269-alien-dictionary/269-alien-dictionary.py
@@ -1,36 +1,39 @@
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
+        
+        # there is a relationship between chars that are implicit
+        # is there a topoligical ordering that we can extract from the words?
+        
+        # t > f?
         graph = {c: set() for word in words for c in word}
         
-        res = list()
         for i in range(len(words) - 1):
             w1, w2 = words[i], words[i + 1]
-            minLength = min(len(w1), len(w2))
             
-            if len(w1) > len(w2) and w1[:minLength] == w2[:minLength]: 
+            min_len = min(len(w1), len(w2))
+            if len(w1) > len(w2) and w1[:min_len] == w2[:min_len]:
                 return ""
-            for j in range(minLength):
-                if w1[j] != w2[j]:    
+            for j in range(min_len):
+                if w1[j] != w2[j]:
                     graph[w1[j]].add(w2[j])
                     break
         
+        
+        res = list()
         visit = {}
         
-        def dfs(node):
-            if node in visit:
-                return visit[node]
-            
-            visit[node] = True
-            for nei in graph[node]:
+        def dfs(c):
+            if c in visit:
+                return visit[c]
+            visit[c] = True
+            for nei in graph[c]:
                 if dfs(nei):
                     return True
-            res.append(node)
-            visit[node] = False
+            res.append(c)
+            visit[c] = False
         
         for c in graph:
-            if dfs(c): 
-                return ""
-        
+            if dfs(c): return ""
+            
         res.reverse()
-        
         return "".join(res)
