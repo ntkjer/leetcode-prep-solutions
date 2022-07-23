770c10a6669dd4fcb7486cf6fa99819d6c6ac8a3
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-07-23 16:30:21 -0700

    Time: 144 ms (25.40%), Space: 15.1 MB (81.62%) - LeetHub

--- 174-dungeon-game/174-dungeon-game.py
+++ 174-dungeon-game/174-dungeon-game.py
@@ -4,29 +4,12 @@ class Solution(object):
         :type dungeon: List[List[int]]
         :rtype: int
         """
-        rows, cols = len(dungeon), len(dungeon[0])
-        dp = [[float('inf')] * cols for _ in range(rows)]
-
-        def get_min_health(currCell, nextRow, nextCol):
-            if nextRow >= rows or nextCol >= cols:
-                return float('inf')
-            nextCell = dp[nextRow][nextCol]
-            # hero needs at least 1 point to survive
-            return max(1, nextCell - currCell)
-
-        for row in reversed(range(rows)):
-            for col in reversed(range(cols)):
-                currCell = dungeon[row][col]
-
-                right_health = get_min_health(currCell, row, col+1)
-                down_health = get_min_health(currCell, row+1, col)
-                next_health = min(right_health, down_health)
-
-                if next_health != float('inf'):
-                    min_health = next_health
-                else:
-                    min_health = 1 if currCell >= 0 else (1 - currCell)
-
-                dp[row][col] = min_health
-
-        return dp[0][0]
+        m, n = len(dungeon), len(dungeon[-1])
+        dp = [float('inf') for _ in range(n + 1)]
+        dp[n - 1] = 1
+        # max(1, (min(dp[j], dp[j + 1]) - dungeon[i][j]));
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                dp[j] = max(1, (min(dp[j], dp[j + 1])) - dungeon[i][j])
+                
+        return dp[0]
