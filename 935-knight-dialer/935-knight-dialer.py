c4d9518a979efa738577229bc963f1b5e94d7cf5
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-07-25 09:14:15 -0700

    Time: 931 ms (78.29%), Space: 13.8 MB (96.07%) - LeetHub

--- 935-knight-dialer/935-knight-dialer.py
+++ 935-knight-dialer/935-knight-dialer.py
@@ -1,24 +1,27 @@
 class Solution:
     def knightDialer(self, n: int) -> int:
-        max_int = (10**9 + 7)
-        moves = {0: (4, 6),
-                1: (8, 6),
-                2: (7, 9),
-                3: (8, 4),
-                4: (3, 9, 0), 
-                5: set(),
-                6: (1, 7, 0), 
-                7: (2, 6),
-                8: (1, 3),
-                9: (2, 4)}
-    
-        current_counts = [1] * 10 # count each num from 0 -> 9
-        for _ in range(n - 1):
-            next_counts = [0] * 10
-            for src_key in range(10):
-                for dst in moves[src_key]:
-                    next_counts[dst] = (next_counts[dst] + current_counts[src_key]) % (max_int)
-                    
-            current_counts = next_counts
+        
+        neighbors = {0: (4, 6), 
+                     1: (8, 6),
+                     2: (7, 9),
+                     3: (8, 4), 
+                     4: (3, 9, 0), 
+                     5: set(),
+                     6: (1, 7, 0),
+                     7: (2, 6),
+                     8: (1, 3),
+                     9: (2, 4)}  
+        
+        
+        counts = [1] * 10
+        
+        max_int = (10 ** 9) + 7
+        for i in range(n - 1):
+            future_counts = [0] * 10
+            for src in range(10):
+                for dst in neighbors[src]:
+                    future_counts[dst] = (future_counts[dst] + counts[src] ) % max_int
+        
+            counts = future_counts
             
-        return sum(current_counts) % max_int
+        return sum(counts) % max_int
