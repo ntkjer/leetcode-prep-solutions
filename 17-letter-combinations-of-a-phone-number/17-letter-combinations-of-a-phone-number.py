e4ca15444680762c045acdb433cdeccc81b115b6
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-07-27 15:03:43 -0700

    Time: 51 ms (40.19%), Space: 14 MB (30.72%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -1,21 +1,24 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if len(digits) == 0: 
-            return ""
-        
-        comb = ('0', '1', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz')
+        if not digits: return []
+        #combinations = ("0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz")
+        res = list()
+        comb = {'0': '0', '1': '1', '2': 'abc', '3':'def', 
+                '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', 
+                '8': 'tuv', '9': 'wxyz'}
 
-        def backtrack(idx, partial):
-            if idx == len(partial):
+        def backtrack(i, partial):
+            if i == len(partial):
                 res.append("".join(partial[:]))
                 return
             
-            for c in comb[int(digits[idx])]:
-                partial[idx] = c
-                backtrack(idx + 1, partial)
-             
+            for c in comb[digits[i]]:
+                partial[i] = c
+                backtrack(i + 1, partial)
                 
-        res = list()
-        partial = ['0' for _ in range(len(digits))]
+
+            
+            
+        partial = ["0" for _ in range(len(digits))]
         backtrack(0, partial)
         return res
