fafdb3b34e74de8965113ae7c063260fbb00b0ec
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-07-27 17:53:05 -0700

    Time: 322 ms (64.39%), Space: 24 MB (82.68%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.py
+++ 1396-design-underground-system/1396-design-underground-system.py
@@ -1,24 +1,24 @@
 class UndergroundSystem:
 
     def __init__(self):
-        self.customers = {} # id: (stationName, t)
-        self.metrics = {}  # (start,end) : (elapsedTime, frequency)
+        self.transactions = {} # id: station, time
+        self.metrics = {} # (start,end) : (totalTime, freq)
 
     def checkIn(self, id: int, stationName: str, t: int) -> None:
-        self.customers[id] = self.customers.get(id, []) + [stationName, t]
-
+        self.transactions[id] = self.transactions.get(id, []) + [stationName, t]
+          
     def checkOut(self, id: int, stationName: str, t: int) -> None:
-        prevStation, prevTime = self.customers[id]
-        del (self.customers[id])
+        prevStation, prevTime = self.transactions[id]
+        del self.transactions[id]
         
         key = tuple([prevStation, stationName])
-        timeDelta = t - prevTime
+        elapsedTime = t - prevTime
         if key in self.metrics:
-            self.metrics[key][0] += timeDelta
+            self.metrics[key][0] += elapsedTime
             self.metrics[key][1] += 1
         else:
-            self.metrics[key] = [timeDelta, 1]
-            
+            self.metrics[key] = [elapsedTime, 1]
+
     def getAverageTime(self, startStation: str, endStation: str) -> float:
         key = tuple([startStation, endStation])
         return self.metrics[key][0] / self.metrics[key][1]
