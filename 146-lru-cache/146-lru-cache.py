2774a6d0d1b4c9c4f6055b5615c1da565ab6360f
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-07-28 14:21:46 -0700

    Time: 1202 ms (57.88%), Space: 76.7 MB (10.75%) - LeetHub

--- 146-lru-cache/146-lru-cache.py
+++ 146-lru-cache/146-lru-cache.py
@@ -1,57 +1,50 @@
-class ListNode:
-    """
-    Doubly linked-list.
-    """
-    def __init__(self, key=None, val=None, nxt=None, prev=None):
+class Node:
+    def __init__(self, key=None, val=None, prev=None, next=None):
         self.key = key
         self.val = val
-        self.next = nxt
+        self.next = next
         self.prev = prev
 
-        
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cache = {}
         self.capacity = capacity
-        self.left = ListNode()
-        self.right = ListNode()
+        self.cache = {}
+        self.left = Node()
+        self.right = Node()
         self.left.next = self.right
         self.right.prev = self.left
-    
-    def _delete(self, node: ListNode):
-        prev, nxt = node.prev, node.next
-        prev.next = nxt
-        nxt.prev = prev
-    
-    def _insert(self, node: ListNode):
-        prev, nxt = self.right.prev, self.right
-        prev.next = node 
-        node.next = nxt
-        node.prev = prev
-        nxt.prev = node
+
+    def _delete(self, node: Node):
+        prv, nxt = node.prev, node.next
+        prv.next, nxt.prev = nxt, prv
+        
+    def _insert(self, node: Node):
+        prv, nxt = self.right.prev, self.right
+        node.next, node.prev = nxt, prv
+        prv.next, nxt.prev = node, node
+        
         
     def get(self, key: int) -> int:
         if key not in self.cache:
             return -1
         self._delete(self.cache[key])
-        self._insert(self.cache[key]) # updates most recent
+        self._insert(self.cache[key])
         return self.cache[key].val
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self._delete(self.cache[key])
-            #del self.cache[key]
             
-        node = ListNode(key, value)
-        self._insert(node)
-        self.cache[key] = node
+        curr = Node(key, value)
+        self._insert(curr)
+        self.cache[key] = curr
         
         if len(self.cache) > self.capacity:
-            # leftmost node is going to be evicted
-            evictedNode = self.left.next
-            self._delete(evictedNode)
-            del self.cache[evictedNode.key]
+            lru = self.left.next
+            self._delete(lru)
+            del self.cache[lru.key]
+        
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
