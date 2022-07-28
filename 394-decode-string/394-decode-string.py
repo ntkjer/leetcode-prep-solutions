2acaf62ac2432cf18684646dcb1b130f0417f00a
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-07-28 14:52:36 -0700

    Time: 48 ms (44.24%), Space: 13.8 MB (71.03%) - LeetHub

--- 394-decode-string/394-decode-string.py
+++ 394-decode-string/394-decode-string.py
@@ -1,17 +1,26 @@
 class Solution:
     def decodeString(self, s: str) -> str:
+        # perhaps a stack?
+        # aaa2[b
+        
+        # tmp = bc
+        # tmp = bcbc
+        # aaabcbc
+        
         stack = []
         for c in s:
-            if c == "]":
-                substring = ""
+            if c == "]": # eval
+                tmp = ""
                 while stack and stack[-1] != "[":
-                    substring = stack.pop() + substring
-                stack.pop()
+                    tmp = stack.pop() + tmp
+                stack.pop() # remove "["
+                
                 freq = ""
                 while stack and stack[-1].isdigit():
                     freq = stack.pop() + freq
-                stack.append(int(freq) * substring)
                 
+                tmp = int(freq) * tmp
+                stack.append(tmp)
             else:
                 stack.append(c)
                 
