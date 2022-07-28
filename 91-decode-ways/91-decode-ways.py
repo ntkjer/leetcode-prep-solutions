20aee7120b4adca32b43254e7ba59750356ac2d9
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-07-27 17:01:09 -0700

    Time: 54 ms (41.37%), Space: 14.1 MB (30.30%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,9 +1,9 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         
-        # decoding for first value + second
         dp = [0 for _ in range(len(s) + 1)]
         dp[0] = 1
+        
         dp[1] = 1 if s[0] != "0" else 0
         
         for i in range(2, len(dp)):
@@ -11,9 +11,10 @@ class Solution:
             pick_one = int(s[i - 1: i])
             if 1 <= pick_one <= 10:
                 dp[i] += dp[i - 1]
-                
+            
             pick_two = int(s[i - 2: i])
             if 10 <= pick_two <= 26:
                 dp[i] += dp[i - 2]
-                
+            
+            print(pick_one, pick_two)
         return dp[-1]
