6046862b5a21b8937982629297a600a75f97398a
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-07-28 16:15:40 -0700

    Time: 994 ms (77.33%), Space: 14 MB (82.15%) - LeetHub

--- 935-knight-dialer/935-knight-dialer.py
+++ 935-knight-dialer/935-knight-dialer.py
@@ -1,27 +1,20 @@
 class Solution:
     def knightDialer(self, n: int) -> int:
         
-        neighbors = {0: (4, 6), 
-                     1: (8, 6),
-                     2: (7, 9),
-                     3: (8, 4), 
-                     4: (3, 9, 0), 
-                     5: set(),
-                     6: (1, 7, 0),
-                     7: (2, 6),
-                     8: (1, 3),
-                     9: (2, 4)}  
+        
+        moves = {0: [4, 6], 1: [6, 8], 2: [7, 9], 
+                 3: [4, 8], 4: [0, 3, 9], 5: [], 
+                 6: [0, 1, 7], 7: [6, 2], 8: [1, 3], 9: [4, 2]}
         
         
-        counts = [1] * 10
+        total_moves = [1] * 10
         
         max_int = (10 ** 9) + 7
-        for i in range(n - 1):
-            future_counts = [0] * 10
-            for src in range(10):
-                for dst in neighbors[src]:
-                    future_counts[dst] = (future_counts[dst] + counts[src] ) % max_int
         
-            counts = future_counts
-            
-        return sum(counts) % max_int
+        for i in range(n - 1):
+            curr_move = [0] * 10
+            for src in range(10):
+                for dst in moves[src]:
+                    curr_move[dst] = (total_moves[src] + curr_move[dst]) % max_int                    
+            total_moves = curr_move
+        return sum(total_moves) % max_int
