04bbe513e02f7be998d184288f087cee4aab409b
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-07-28 17:26:06 -0700

    Time: 64 ms (38.92%), Space: 14.3 MB (9.47%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -1,22 +1,22 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        res = list()
-        nums.sort()
         
+        nums.sort()
+        res = list()
+    
         def backtrack(i, partial):
             if i == len(nums):
                 res.append(partial[:])
                 return
-            
+        
             partial.append(nums[i])
             backtrack(i + 1, partial)
             partial.pop()
             
-            # remove duplicate entries
             while i + 1 < len(nums) and nums[i] == nums[i + 1]:
                 i += 1
-            
+                
             backtrack(i + 1, partial)
-        
+            
         backtrack(0, [])
         return res
