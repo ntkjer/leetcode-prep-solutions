3f7bfd880231f7e77970adfb9c35a4dc26df936e
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-07-28 18:50:35 -0700

    Time: 54 ms (74.16%), Space: 14.1 MB (22.97%) - LeetHub

--- 1274-number-of-ships-in-a-rectangle/1274-number-of-ships-in-a-rectangle.py
+++ 1274-number-of-ships-in-a-rectangle/1274-number-of-ships-in-a-rectangle.py
@@ -12,23 +12,21 @@
 
 class Solution:
     def countShips(self, sea: 'Sea', topRight: 'Point', bottomLeft: 'Point') -> int:
+        
         if bottomLeft.x > topRight.x or bottomLeft.y > topRight.y:
             return 0
         if not sea.hasShips(topRight, bottomLeft):
             return 0
-        if topRight.x == bottomLeft.x and topRight.y == bottomLeft.y:
+        
+        elif bottomLeft.x == topRight.x and bottomLeft.y == topRight.y:
             return 1
+            
+        midX = (bottomLeft.x + topRight.x) // 2
+        midY = (bottomLeft.y + topRight.y) // 2
         
-        midX = (topRight.x + bottomLeft.x ) // 2
-        midY = (topRight.y + bottomLeft.y) // 2
-        
-        
-        # bottomLeftRect = bottomLeft, Point(midX, midY)
-        # bottomRightRect = Point(midX, bottomLeft.y), Point(bottomLeft.x, midY)
-        # topLeftRect = Point(bottomLeft.x, midY + 1),Point(midX + 1, topRight.y)
-        # topRightRect = Point(midX + 1, midY + 1), topRight
-        rect1 = self.countShips(sea, Point(midX, midY), bottomLeft) # bottomLeft square
-        rect2 = self.countShips(sea, Point(midX, topRight.y), Point(bottomLeft.x, midY + 1)) # topLeft square
-        rect3 = self.countShips(sea, Point(topRight.x, midY), Point(midX + 1, bottomLeft.y)) # bottomRight squazre
-        rect4 = self.countShips(sea, topRight, Point(midX + 1, midY + 1)) # topRight 
+        rect1 = self.countShips(sea, Point(midX, midY), bottomLeft) #bottom left
+        rect2 = self.countShips(sea, Point(topRight.x, midY), Point(midX + 1, bottomLeft.y)) #bottom right
+        rect3 = self.countShips(sea, Point(midX, topRight.y), Point(bottomLeft.x, midY + 1)) # upper left
+        rect4 = self.countShips(sea, topRight, Point(midX + 1, midY + 1)) # upper right
         return rect1 + rect2 + rect3 + rect4
+
