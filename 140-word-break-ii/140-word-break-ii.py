afce38cd046af664724cf7f98b5d436625758e02
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-07-28 17:05:58 -0700

    Time: 59 ms (17.82%), Space: 14 MB (33.67%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.py
+++ 140-word-break-ii/140-word-break-ii.py
@@ -1,9 +1,9 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
         
+        
         trie = {}
         end = "$"
-        
         for word in wordDict:
             root = trie
             for ch in word:
@@ -13,25 +13,24 @@ class Solution:
             root[end] = word
         
         res = list()
-        
         def backtrack(word=s, partial=[]):
             root = trie
-            for idx, char in enumerate(word):
+            
+            for idx, ch in enumerate(word):
                 if end in root:
                     partial.append(root[end])
                     backtrack(word[idx:], partial)
                     partial.pop()
                     
-                if char not in root:
+                if ch not in root:
                     return
-                  
-                root = root[char]
+                
+                root = root[ch]
+                
             if end in root:
                 partial.append(root[end])
-                res.append(" ".join(partial))
+                res.append(" ".join(partial[:]))
                 partial.pop()
                 
-            
-        root = trie
         backtrack()
         return res
