d433ecb3a239296dd0e8680e3210960bd5e6a666
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-08-02 09:30:02 -0700

    Time: 157 ms (40.44%), Space: 16.8 MB (54.31%) - LeetHub

--- 348-design-tic-tac-toe/348-design-tic-tac-toe.py
+++ 348-design-tic-tac-toe/348-design-tic-tac-toe.py
@@ -1,27 +1,24 @@
 class TicTacToe:
 
     def __init__(self, n: int):
+        self.n = n
         self.rows = [0] * n
         self.cols = [0] * n
-        self.n = n
-        self.diag1 = 0
-        self.diag2 = 0
-
+        self.diag = 0
+        self.aDiag = 0
+        
     def move(self, row: int, col: int, player: int) -> int:
         n = self.n
-        move=1
-        if player == 2:
-            move = -1
+        self.rows[row] += 1 if player == 1 else -1
+        self.cols[col] += 1 if player == 1 else -1
         
-        self.rows[row] += move
-        self.cols[col] += move
+        if row + col == (n - 1):
+            self.aDiag += 1 if player == 1 else -1
         
         if row == col:
-            self.diag1 += move
-        if row + col == (n - 1):
-            self.diag2 += move
+            self.diag += 1 if player == 1 else -1
         
-        if abs(self.cols[col]) == n or abs(self.rows[row]) == n or abs(self.diag1) == n or abs(self.diag2) == n:
+        if abs(self.rows[row]) == n or abs(self.cols[col]) == n or abs(self.diag) == n or abs(self.aDiag) == n:
             return player
         return 0
 
