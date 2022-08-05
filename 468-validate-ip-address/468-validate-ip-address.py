a5466168249b721b359746fc626823779d478566
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-08-05 08:25:08 -0700

    Time: 75 ms (5.43%), Space: 13.8 MB (99.06%) - LeetHub

--- 468-validate-ip-address/468-validate-ip-address.py
+++ 468-validate-ip-address/468-validate-ip-address.py
@@ -1,33 +1,34 @@
-from ipaddress import ip_address, IPv6Address
-
 class Solution:
     def validIPAddress(self, queryIP: str) -> str:
-        
-        def validate_ipv4(ip):
-            nums = ip.split('.')
-            for x in nums:
+        def isValidIPv4(ip):
+            ip = ip.split(".")
+            for x in ip:
                 if len(x) == 0 or len(x) > 3:
-                    return "Neither"
-                if x[0] == '0' and len(x) != 1 or not x.isdigit() or int(x) > 255:
-                    return "Neither"
+                    return False
+                if x[0] == "0" and len(x) != 1 or \
+                not x.isdigit() or int(x) > 255:
+                    return False
+            return True
+        
+        def isValidIPv6(ip):
+            hexChars = "1234567890abcdefABCDEF"
+            hexRange = set([x for x in hexChars])
+            ip = ip.split(":")
+            if len(ip) < 8:
+                return False
+            for block in ip:
+                if not (1 <= len(block) <= 4):
+                    return False
+                for c in block:
+                    if c not in hexRange:
+                        return False
+            return True
+        
+        
+        if queryIP.count(".") == 3 and isValidIPv4(queryIP):
             return "IPv4"
-        
-        def validate_ipv6(ip):
-            nums = ip.split(':')
-            if len(nums) < 8:
-                return "Neither"
-            hexadigits = "0123456789abcdefABCDEF"
-            hexa = set(digit for digit in hexadigits)
-            for x in nums:
-                if len(x) == 0 or len(x) > 4:
-                    return "Neither"
-                for c in x:
-                    if c not in hexa: return "Neither"
+        elif queryIP.count(":") == 7 and isValidIPv6(queryIP):
             return "IPv6"
-        
-        if queryIP.count('.') == 3:
-            return validate_ipv4(queryIP)
-        elif queryIP.count(':') == 7:
-            return validate_ipv6(queryIP)
         else:
             return "Neither"
+        
