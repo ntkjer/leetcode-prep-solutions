183030fbeaf118403119bd3b2065803a25b655a9
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-08-05 07:30:00 -0700

    Time: 129 ms (29.70%), Space: 14.1 MB (99.06%) - LeetHub

--- 271-encode-and-decode-strings/271-encode-and-decode-strings.py
+++ 271-encode-and-decode-strings/271-encode-and-decode-strings.py
@@ -2,30 +2,34 @@ class Codec:
     def encode(self, strs: List[str]) -> str:
         """Encodes a list of strings to a single string.
         """
+        # number, delimeter, word
         res = list()
-        delim = "$"
-        for w in strs:
-            res.append(str(len(w)) + delim + w)
+        for word in strs:
+            res.append(str(len(word)) + "$" + word)
         return "".join(res)
+        
 
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
-        delim = "$"
-        N = len(s)
         i = 0
         j = 0
+        n = len(s)
         res = list()
-        while i < N:
-            while s[i] != delim:
+        
+        while i < n:
+            while s[i] != "$":
                 i += 1
-    
+            
             length = int(s[j: i])
             
-            res.append(s[i + 1: length + i + 1])
+            res.append(s[i + 1: i + length + 1])
+        
             i = length + i + 1
             j = i
+            
         return res
+        
 
 
 # Your Codec object will be instantiated and called as such:
