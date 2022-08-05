7b2cadee6085fb52e1c0a77e863dd85ca9914b23
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-08-05 09:11:26 -0700

    Time: 105 ms (69.89%), Space: 14.1 MB (31.78%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -2,38 +2,38 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2): 
             return False
+        s1Freq = [0] * 26
+        s2Freq = [0] * 26
         
-        s1Count, s2Count = [0] * 26, [0] * 26
-        matches = 0
         
         for i in range(len(s1)):
-            s1Count[ord(s1[i]) - ord('a')] += 1
-            s2Count[ord(s2[i]) - ord('a')] += 1
+            s1Freq[ord(s1[i]) - ord('a')] += 1
+            s2Freq[ord(s2[i]) - ord('a')] += 1
         
-        for i in range(len(s1Count)):
-            if s1Count[i] == s2Count[i]:
+        matches = 0
+        for i in range(len(s1Freq)):
+            if s1Freq[i] == s2Freq[i]:
                 matches += 1
                 
         l = 0
-        
         for r in range(len(s1), len(s2)):
             if matches == 26:
                 return True
             
             idx = ord(s2[r]) - ord('a')
-            s2Count[idx] += 1
-            if s2Count[idx] == s1Count[idx]:
+            s2Freq[idx] += 1
+            if s2Freq[idx] == s1Freq[idx]:
                 matches += 1
-            elif s2Count[idx] == s1Count[idx] + 1:
+            elif s2Freq[idx] == s1Freq[idx] + 1:
                 matches -= 1
+            
             
             idx = ord(s2[l]) - ord('a')
-            s2Count[idx] -= 1
-            if s2Count[idx] == s1Count[idx]:
+            s2Freq[idx] -= 1
+            if s2Freq[idx] == s1Freq[idx]:
                 matches += 1
-            elif s2Count[idx] == s1Count[idx] - 1:
+            elif s2Freq[idx] == s1Freq[idx] - 1:
                 matches -= 1
-                
-            l += 1
             
+            l += 1
         return matches == 26
