1f4193a6fea717be5b2ae30022d082c8e0a4e62b
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-08-10 11:02:15 -0700

    Time: 43 ms (61.34%), Space: 14 MB (36.05%) - LeetHub

--- 468-validate-ip-address/468-validate-ip-address.py
+++ 468-validate-ip-address/468-validate-ip-address.py
@@ -1,34 +1,33 @@
 class Solution:
     def validIPAddress(self, queryIP: str) -> str:
-        def isValidIPv4(ip):
+        
+        def validIPv4(ip):
             ip = ip.split(".")
             for x in ip:
                 if len(x) == 0 or len(x) > 3:
                     return False
-                if x[0] == "0" and len(x) != 1 or \
-                not x.isdigit() or int(x) > 255:
+                if x[0] == "0" and len(x) != 1 or not x.isdigit() or int(x) > 255:
                     return False
             return True
         
-        def isValidIPv6(ip):
-            hexChars = "1234567890abcdefABCDEF"
-            hexRange = set([x for x in hexChars])
+        def validIPv6(ip):
+            hexChars = "abcdefABCDEF0123456789"
+            hexSet = set([c for c in hexChars])
             ip = ip.split(":")
+            
             if len(ip) < 8:
                 return False
             for block in ip:
                 if not (1 <= len(block) <= 4):
                     return False
                 for c in block:
-                    if c not in hexRange:
+                    if c not in hexSet:
                         return False
             return True
         
-        
-        if queryIP.count(".") == 3 and isValidIPv4(queryIP):
+        if queryIP.count('.') == 3 and validIPv4(queryIP):
             return "IPv4"
-        elif queryIP.count(":") == 7 and isValidIPv6(queryIP):
+        elif queryIP.count(':') == 7 and validIPv6(queryIP):
             return "IPv6"
         else:
             return "Neither"
-        
