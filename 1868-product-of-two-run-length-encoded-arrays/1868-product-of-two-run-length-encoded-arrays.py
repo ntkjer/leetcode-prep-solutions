d0794b1ba3eafbd310dc11a36e8212525a151fc8
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-08-11 05:04:56 -0700

    Time: 5995 ms (5.36%), Space: 73.8 MB (7.05%) - LeetHub

--- 1868-product-of-two-run-length-encoded-arrays/1868-product-of-two-run-length-encoded-arrays.py
+++ 1868-product-of-two-run-length-encoded-arrays/1868-product-of-two-run-length-encoded-arrays.py
@@ -1,21 +1,21 @@
 class Solution:
     def findRLEArray(self, encoded1: List[List[int]], encoded2: List[List[int]]) -> List[List[int]]:
         
-        i = j = 0
-        f1 = 0
-        f2 = 0
-        v1 = v2 = 0
+        res = [] # num, freq
+        f1, f2 = 0, 0
+        v1, v2 = 0, 0
+        i, j = 0, 0
+        
         m, n = len(encoded1), len(encoded2)
-        res = list()
         
         while i < m and j < n:
-            
             if not f1 and i < m:
                 v1, f1 = encoded1[i]
             if not f2 and j < n:
                 v2, f2 = encoded2[j]
-            
-            cur_min, product = min(f1, f2), v1 * v2
+                
+            cur_min = min(f1, f2)
+            product = v1 * v2
             
             if res and res[-1][0] == product:
                 res[-1][1] += cur_min
@@ -29,5 +29,4 @@ class Solution:
                 i += 1
             if not f2:
                 j += 1
-                
-        return res
+        return res 
