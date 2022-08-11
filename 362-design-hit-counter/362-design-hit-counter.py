4190bca530f3f180ab6822454de872946a076966
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-08-11 04:18:12 -0700

    Time: 60 ms (20.09%), Space: 13.9 MB (61.33%) - LeetHub

--- 362-design-hit-counter/362-design-hit-counter.py
+++ 362-design-hit-counter/362-design-hit-counter.py
@@ -2,10 +2,10 @@ class HitCounter:
 
     def __init__(self):
         self.data = collections.deque()
-
+        
     def hit(self, timestamp: int) -> None:
         self.data.append(timestamp)
-        
+
     def getHits(self, timestamp: int) -> int:
         period = 60 * 5
         while self.data and timestamp - self.data[0] >= period:
