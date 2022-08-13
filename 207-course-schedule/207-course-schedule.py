e84c89a9591e82d545853d72a058615db2c47141
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-08-13 10:17:23 -0700

    Time: 165 ms (46.22%), Space: 17.8 MB (6.16%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,24 +1,25 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         
-        adj_list = {i: set() for i in range(numCourses)}
+        graph = {i: set() for i in range(numCourses)}
         
-        for course, pre in prerequisites:
-            adj_list[course].add(pre)
-            
+        for course, req in prerequisites:
+            graph[course].add(req)
+        
         visit = {}
-        def has_cycle(course):
+        def dfs(course):
             if course in visit:
                 return visit[course]
-            visit[course] = True
-            for pre in adj_list[course]:
-                if has_cycle(pre): 
-                    return True
-            visit[course] = False
             
+            visit[course] = True
+            for nei in graph[course]:
+                if dfs(nei):
+                    return True
+                
+            visit[course] = False
         
-        for i in range(numCourses):
-            if has_cycle(i): 
+        for course in range(numCourses):
+            if dfs(course):
                 return False
-        
+            
         return True
