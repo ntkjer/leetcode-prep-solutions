e140cbf8d1497cbb8293257d4f6a74000836e032
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-08-15 11:46:45 -0700

    Time: 151 ms (32.69%), Space: 14.4 MB (79.17%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.py
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -1,22 +1,13 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         
-        # max can be the product of two numbers
-        # need to keep track of global max, curent_max, and negative_max?
-        
-        cur_max = nums[0]
         res = nums[0]
-        cur_min = nums[0]
+        minProd = nums[0]
+        maxProd = nums[0]
         
         for num in nums[1:]:
-            tmp = max(num, cur_max * num, cur_min * num) 
-            cur_min = min(num, cur_min * num, cur_max * num)
-            cur_max = tmp
-            res = max(res, tmp)
-        
-        
-        #           [-2, 0, -1]
-        # cur_max = -2, 0, 2
-        # res     = -2, 0
-        # cur_min = -2, -2
+            tmp = max(num, num * minProd, num * maxProd)
+            minProd = min(num, num * minProd, num * maxProd)
+            maxProd = tmp
+            res = max(res, maxProd)
         return res
