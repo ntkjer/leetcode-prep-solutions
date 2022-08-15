32689aafdfa2ad97120b57a9e7d7d81c2457d085
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-08-15 13:05:42 -0700

    Time: 55 ms (76.40%), Space: 14.1 MB (70.87%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.py
+++ 97-interleaving-string/97-interleaving-string.py
@@ -1,28 +1,29 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        m, n = len(s1), len(s2)
-        if m + n != len(s3):
+        if len(s1) + len(s2) != len(s3):
             return False
         
+        m, n = len(s1), len(s2)
         dp = [[False for _ in range(n + 1)] for _ in range(m + 1)]
         
         dp[0][0] = True
         
         for i in range(1, m + 1):
             dp[i][0] = dp[i - 1][0] and s1[i - 1] == s3[i - 1]
-            
-        for i in range(1, n + 1):
-            dp[0][i] = dp[0][i - 1] and s2[i - 1] == s3[i - 1]
         
+        for j in range(1, n + 1):
+            dp[0][j] = dp[0][j - 1] and s2[j - 1] == s3[j - 1]
+            
         for i in range(1, m + 1):
             for j in range(1, n + 1):
-                chooseS1, chooseS2 = False, False
+                chooseS1 = False
+                chooseS2 = False
+                
                 if s1[i - 1] == s3[i + j - 1]:
                     chooseS1 = dp[i - 1][j]
                 if s2[j - 1] == s3[i + j - 1]:
                     chooseS2 = dp[i][j - 1]
-                    
+                
                 dp[i][j] = chooseS1 or chooseS2
         
         return dp[m][n]
-                    
