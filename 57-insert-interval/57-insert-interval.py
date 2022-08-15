19aca0c69fe24567b10623f415083ef812f5b226
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-08-15 13:50:20 -0700

    Time: 171 ms (10.98%), Space: 17.5 MB (15.93%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -1,19 +1,17 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        
+        idx = 0
         N = len(intervals)
         res = list()
-        idx = 0
-        
         while idx < N and newInterval[0] > intervals[idx][1]:
             res.append(intervals[idx])
             idx += 1
-            
-        # intervals are now intersecting
+        
         while idx < N and newInterval[1] >= intervals[idx][0]:
             newInterval = [min(newInterval[0], intervals[idx][0]), max(newInterval[1], intervals[idx][1])]
             idx += 1
-        
-        
+            
         
         return res + [newInterval] + intervals[idx:]
-        
+            
