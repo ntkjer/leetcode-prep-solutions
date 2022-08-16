87ce85f7708cc8c42fd42a3b26b3bf9d8f9d51ee
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-08-16 10:26:24 -0700

    Time: 170 ms (10.68%), Space: 15.2 MB (54.89%) - LeetHub

--- 174-dungeon-game/174-dungeon-game.py
+++ 174-dungeon-game/174-dungeon-game.py
@@ -1,30 +1,29 @@
 class Solution:
     def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
-        
         m, n = len(dungeon), len(dungeon[-1])
         
-        def valid_pos(r, c):
-            if not (0 <= r < m) or not(0 <= c < n):
+        def is_valid_pos(r, c):
+            if not (0 <= r < m) or not(0 <= c < n): 
                 return False
             return True
         
+        
         dp = [[float('inf') for _ in range(n)] for _ in range(m)]
         
-        dp[-1][-1] = 1
-        
-        # base case 
         for i in range(m - 1, -1, -1):
             for j in range(n - 1, -1, -1):
                 
+                up = float('inf')
+                left = float('inf')
+                
                 curr = dungeon[i][j]
-                up, left = float('inf'), float('inf')
                 
-                if valid_pos(i + 1, j):
+                if is_valid_pos(i + 1, j):
                     up = max(dp[i + 1][j] - curr, 1)
-                
-                if valid_pos(i, j + 1):
+                if is_valid_pos(i, j + 1):
                     left = max(dp[i][j + 1] - curr, 1)
-        
+                
+                
                 candidate = min(up, left)
                 if candidate != float('inf'):
                     dp[i][j] = candidate
@@ -32,6 +31,6 @@ class Solution:
                     if curr >= 0:
                         dp[i][j] = 1
                     else:
-                        dp[i][j] = 1 - curr      
+                        dp[i][j] = 1 - curr
         
         return dp[0][0]
