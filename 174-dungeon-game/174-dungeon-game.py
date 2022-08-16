24f062fc2ceb256334e596932b42aa152cee3af3
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-08-16 10:15:32 -0700

    Time: 176 ms (8.38%), Space: 15.2 MB (54.89%) - LeetHub

--- 174-dungeon-game/174-dungeon-game.py
+++ 174-dungeon-game/174-dungeon-game.py
@@ -1,40 +1,37 @@
 class Solution:
     def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
         
+        m, n = len(dungeon), len(dungeon[-1])
         
-        rows, cols = len(dungeon), len(dungeon[-1])
-        
-        dp = [[float('inf') for _ in range(cols)] for _ in range(rows)]
-
-        def valid_pos(i, j):
-            if not (0 <= i < rows) or not (0 <= j < cols):
+        def valid_pos(r, c):
+            if not (0 <= r < m) or not(0 <= c < n):
                 return False
             return True
         
-        for i in range(rows - 1, -1, -1):
-            for j in range(cols - 1, -1, -1):
+        dp = [[float('inf') for _ in range(n)] for _ in range(m)]
+        
+        dp[-1][-1] = 1
+        
+        # base case 
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
                 
-                curr_cell = dungeon[i][j]
-                
+                curr = dungeon[i][j]
+                up, left = float('inf'), float('inf')
                 
                 if valid_pos(i + 1, j):
-                    up = max(dp[i + 1][j] - curr_cell, 1)  
-                else:
-                    up = float('inf')
+                    up = max(dp[i + 1][j] - curr, 1)
                 
                 if valid_pos(i, j + 1):
-                    down = max(dp[i][j + 1] - curr_cell, 1)
-                else:
-                    down = float('inf')
-                
-                candidate = min(up, down)
+                    left = max(dp[i][j + 1] - curr, 1)
+        
+                candidate = min(up, left)
                 if candidate != float('inf'):
                     dp[i][j] = candidate
                 else:
-                    if curr_cell >= 0:
+                    if curr >= 0:
                         dp[i][j] = 1
                     else:
-                        dp[i][j] = 1 - curr_cell
-                        
+                        dp[i][j] = 1 - curr      
         
         return dp[0][0]
