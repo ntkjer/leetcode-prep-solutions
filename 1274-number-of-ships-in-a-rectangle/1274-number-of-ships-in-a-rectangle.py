817e6bf64412a54b7875d70ec4df77de6ac4666f
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-08-16 09:57:19 -0700

    Time: 69 ms (43.43%), Space: 14.1 MB (19.19%) - LeetHub

--- 1274-number-of-ships-in-a-rectangle/1274-number-of-ships-in-a-rectangle.py
+++ 1274-number-of-ships-in-a-rectangle/1274-number-of-ships-in-a-rectangle.py
@@ -13,20 +13,20 @@
 class Solution:
     def countShips(self, sea: 'Sea', topRight: 'Point', bottomLeft: 'Point') -> int:
         
+        
         if bottomLeft.x > topRight.x or bottomLeft.y > topRight.y:
             return 0
-        if not sea.hasShips(topRight, bottomLeft):
+        elif not sea.hasShips(topRight, bottomLeft):
             return 0
-        
         elif bottomLeft.x == topRight.x and bottomLeft.y == topRight.y:
             return 1
-            
-        midX = (bottomLeft.x + topRight.x) // 2
-        midY = (bottomLeft.y + topRight.y) // 2
         
-        rect1 = self.countShips(sea, Point(midX, midY), bottomLeft) #bottom left
-        rect2 = self.countShips(sea, Point(topRight.x, midY), Point(midX + 1, bottomLeft.y)) #bottom right
-        rect3 = self.countShips(sea, Point(midX, topRight.y), Point(bottomLeft.x, midY + 1)) # upper left
-        rect4 = self.countShips(sea, topRight, Point(midX + 1, midY + 1)) # upper right
-        return rect1 + rect2 + rect3 + rect4
-
+        midX = (topRight.x + bottomLeft.x) // 2
+        midY = (topRight.y + bottomLeft.y) // 2
+    
+        lowerLeftQuad = self.countShips(sea, Point(midX, midY), bottomLeft)
+        lowerRightQuad = self.countShips(sea, Point(topRight.x, midY), Point(midX + 1, bottomLeft.y))
+        upperLeftQuad = self.countShips(sea, Point(midX, topRight.y), Point(bottomLeft.x, midY + 1))
+        upperRightQuad = self.countShips(sea, topRight, Point(midX + 1, midY + 1))
+        
+        return lowerLeftQuad + lowerRightQuad + upperLeftQuad + upperRightQuad
