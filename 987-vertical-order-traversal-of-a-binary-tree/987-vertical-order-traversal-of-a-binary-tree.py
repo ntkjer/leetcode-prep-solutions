f180225e511521100ed2d3606f371994cc167407
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-08-17 04:42:00 -0700

    Time: 34 ms (95.36%), Space: 14.3 MB (5.83%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.py
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.py
@@ -8,34 +8,35 @@ class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
         
         columns = {}
-        
         q = collections.deque()
-        q.append((root, 0, 0))
+        max_level = 0
+        min_level = 0
         
-        min_col, max_col = 0, 0
+        q.append((root, 0, 0))
         nodes = list()
+        
+        
         while q:
+            node, col, level = q.popleft()
             
-            node, col, row = q.popleft()
-            
-            if not node: 
+            if not node:
                 continue
             
-            nodes.append((col, row, node.val))
-            q.append((node.left, col - 1, row + 1))
-            q.append((node.right, col + 1, row + 1))
+            nodes.append([col, level, node.val])
             
+            q.append((node.left, col - 1, level + 1))
+            q.append((node.right, col + 1, level + 1))
+        
         nodes.sort()
-        node_dict = {}
+        res = {}
         
-        for col, row, value in nodes:
-            min_col = min(col, min_col)
-            max_col = max(col, max_col)
-            
-            node_dict[col] = node_dict.get(col, []) + [value]
+        for col, level, val in nodes:
+            res[col] = res.get(col, []) + [val]
         
-        res = list()
-        for i in range(min_col, max_col + 1):
-            res.append(node_dict[i])
-            
-        return res
+        return res.values()
+        
+        
+        
+        
+        
+        
