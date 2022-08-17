302d209c3aa09fe7c0c01d9edb70ebc777c1a008
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-08-17 12:35:12 -0700

    Time: 58 ms (54.04%), Space: 14.2 MB (49.70%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -1,7 +1,9 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        
         res = list()
         nums.sort()
+        
         def backtrack(i=0, partial=[]):
             if i == len(nums):
                 res.append(partial[:])
@@ -12,9 +14,8 @@ class Solution:
             partial.pop()
             while (i + 1 < len(nums) and nums[i] == nums[i + 1]):
                 i += 1
-                
+            
             backtrack(i + 1, partial)
-        
-                
+            
         backtrack()
         return res
