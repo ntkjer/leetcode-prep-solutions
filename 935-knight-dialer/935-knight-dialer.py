1f96312708960ef8e9679d8d9510fecfdb2c4963
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-08-17 08:37:40 -0700

    Time: 1169 ms (70.67%), Space: 13.9 MB (81.71%) - LeetHub

--- 935-knight-dialer/935-knight-dialer.py
+++ 935-knight-dialer/935-knight-dialer.py
@@ -1,19 +1,16 @@
 class Solution:
     def knightDialer(self, n: int) -> int:
-        
-        moves = {1: [6, 8], 2: [7, 9], 3: [8, 4], 
-                 4: [0, 3, 9], 5: [], 6: [0, 1, 7], 
-                 7: [2, 6], 8: [1, 3], 9: [2, 4], 0: [4, 6]}
-        
         max_int = (10 ** 9) + 7
+        combinations = {0: [4, 6], 1: [6, 8], 2: [7, 9], 
+                        3: [4, 8], 4: [0, 3, 9], 5: [], 
+                        6: [0, 1, 7], 7: [2, 6], 8: [1, 3], 9: [2, 4]}
         
-        prev_state = [1] * 10
+        moves = [1] * 10
         for i in range(n - 1):
-            curr_state = [0] * 10
-            for start in range(10):
-                for end in moves[start]:
-                    curr_state[end] = (prev_state[start] + curr_state[end] ) % max_int
-            prev_state = curr_state
-        
-        return sum(prev_state) % max_int
-                
+            curr_moves = [0] * 10
+            for i in range(10):
+                for next_move in combinations[i]:
+                    curr_moves[next_move] = (curr_moves[next_move] + moves[i]) % max_int
+                    
+            moves = curr_moves
+        return sum(moves) % max_int
