dc3d95db6bfc9d5eff56b1000855528ef18f68ac
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-08-18 16:07:11 -0700

    Time: 191 ms (88.56%), Space: 14.1 MB (19.36%) - LeetHub

--- 723-candy-crush/723-candy-crush.py
+++ 723-candy-crush/723-candy-crush.py
@@ -2,36 +2,35 @@ class Solution:
     def candyCrush(self, board: List[List[int]]) -> List[List[int]]:
         
         rows, cols = len(board), len(board[-1])
-        
         while True:
             crushable = set()
             
-            # mark vertically
-            
+            # add all crushable items top to bottom
             for r in range(rows - 2):
                 for c in range(cols):
                     if board[r][c] and board[r][c] == board[r + 1][c] == board[r + 2][c]:
                         crushable.add((r, c))
                         crushable.add((r + 1, c))
                         crushable.add((r + 2, c))
-                        
-        
-            # mark horizontally
+            
+            # add all crushable items left to right            
             for r in range(rows):
                 for c in range(cols - 2):
                     if board[r][c] and board[r][c] == board[r][c + 1] == board[r][c + 2]:
                         crushable.add((r, c))
                         crushable.add((r, c + 1))
                         crushable.add((r, c + 2))
-                                    
+                        
             if not crushable:
                 return board
             
-            for r,c in crushable:
+            # crush candies
+            for (r, c) in crushable:
                 board[r][c] = 0
-            
+                
             # let gravity fall
             for c in range(cols - 1, -1, -1):
+                # find an offset that covers bottom to top?
                 offset = 0
                 for r in range(rows - 1, -1, -1):
                     k = r + offset
@@ -39,7 +38,10 @@ class Solution:
                         offset += 1
                     else:
                         board[k][c] = board[r][c]
-                for r in range(offset):
-                    board[r][c] = 0            
                 
-            
+                for r in range(offset):
+                    board[r][c] = 0
+                    
+                    
+                
+              
