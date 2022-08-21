d06340d71e5d0f0f28572d2db6fd814aea569cee
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-08-21 12:08:39 -0700

    Time: 751 ms (5.37%), Space: 14.3 MB (96.73%) - LeetHub

--- 1169-invalid-transactions/1169-invalid-transactions.py
+++ 1169-invalid-transactions/1169-invalid-transactions.py
@@ -1,37 +1,40 @@
 class Solution:
     def invalidTransactions(self, transactions: List[str]) -> List[str]:
         
+        
         people = []
         time = []
-        amount = []
         location = []
+        amount = []
         
-        
-        for transaction in transactions:
-            person, t, amt, city = transaction.split(',')
-            people.append(person)
-            time.append(t)
-            amount.append(amt)
+        for i in range(len(transactions)):
+            transaction = transactions[i]
+            name, t, amt, city = transaction.split(",")
+            people.append(name)
+            time.append(int(t))
             location.append(city)
-            
+            amount.append(int(amt))
+        
+        
         n = len(transactions)
         invalid = [False] * n
         
         for i in range(n):
             for j in range(i + 1, n):
-                if int(amount[j]) > 1000:
-                    invalid[j] = True
-                if int(amount[i]) > 1000:
+                if amount[i] > 1000:
                     invalid[i] = True
+                if amount[j] > 1000:
+                    invalid[j] = True
                 if people[i] == people[j]:
-                    time_diff = abs(int(time[j]) - int(time[i]))
+                    time_diff = abs(time[j] - time[i])
                     if time_diff <= 60 and location[i] != location[j]:
                         invalid[i] = True
                         invalid[j] = True
                         
-        res = list()        
+        
+        res = list()
         for i in range(n):
             if invalid[i]:
                 res.append(transactions[i])
-                        
+        
         return res
