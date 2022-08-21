e643c118f93928124c0d8fbf72b16263fc72460d
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-08-21 11:23:02 -0700

    Time: 111 ms (22.22%), Space: 14.2 MB (62.04%) - LeetHub

--- 635-design-log-storage-system/635-design-log-storage-system.py
+++ 635-design-log-storage-system/635-design-log-storage-system.py
@@ -1,27 +1,28 @@
 
+
 class LogSystem:
 
     def __init__(self):
-        self.data = {} # id: [timestamps]
-        self.granularMap = {"Year": 1, "Month": 2, "Day": 3, "Hour": 4, "Minute": 5, "Second": 6}
+        self.logs = collections.defaultdict(int) 
+        self.granularityMap = {"Year": 1, "Month": 2, "Day": 3, "Hour": 4, "Minute": 5, "Second": 6}
         
         
     def put(self, id: int, timestamp: str) -> None:
-        self.data[tuple(timestamp.split(":"))] = id
-        
+        self.logs[tuple(timestamp.split(":"))] = id
+
     def retrieve(self, start: str, end: str, granularity: str) -> List[int]:
-        idx = self.granularMap[granularity]
+        idx = self.granularityMap[granularity]
         
         start = tuple(start.split(":")[:idx])
         end = tuple(end.split(":")[:idx])
         
         res = list()
-        for ts in self.data.keys():
-            if start <= ts[:idx] <= end:
-                res.append(self.data[ts])
-                
-        return res
         
+        for ts in self.logs.keys():
+            if start <= ts[:idx] <= end:
+                res.append(self.logs[ts])
+        
+        return res
 
 
 # Your LogSystem object will be instantiated and called as such:
