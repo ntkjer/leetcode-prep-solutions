2f85ef8731a85e1137a6aafc28b1d70ed927adfe
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-08-22 05:35:57 -0700

    Time: 75 ms (68.06%), Space: 14.3 MB (30.56%) - LeetHub

--- 635-design-log-storage-system/635-design-log-storage-system.py
+++ 635-design-log-storage-system/635-design-log-storage-system.py
@@ -1,27 +1,25 @@
-
-
 class LogSystem:
 
     def __init__(self):
-        self.logs = collections.defaultdict(int) 
         self.granularityMap = {"Year": 1, "Month": 2, "Day": 3, "Hour": 4, "Minute": 5, "Second": 6}
-        
-        
+        self.logs = {} # store timestamp: id
+
     def put(self, id: int, timestamp: str) -> None:
+        key = tuple(timestamp.split(":"))
         self.logs[tuple(timestamp.split(":"))] = id
 
     def retrieve(self, start: str, end: str, granularity: str) -> List[int]:
         idx = self.granularityMap[granularity]
-        
         start = tuple(start.split(":")[:idx])
         end = tuple(end.split(":")[:idx])
+        
         
         res = list()
         
         for ts in self.logs.keys():
             if start <= ts[:idx] <= end:
                 res.append(self.logs[ts])
-        
+                
         return res
 
 
