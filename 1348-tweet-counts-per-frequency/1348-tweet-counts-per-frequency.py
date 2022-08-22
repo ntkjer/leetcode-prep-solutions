20af0630ec979aa3996296c5e5e74b78d6b080cd
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-08-22 08:45:13 -0700

    Time: 343 ms (91.33%), Space: 19.9 MB (83.33%) - LeetHub

--- 1348-tweet-counts-per-frequency/1348-tweet-counts-per-frequency.py
+++ 1348-tweet-counts-per-frequency/1348-tweet-counts-per-frequency.py
@@ -5,17 +5,15 @@ class TweetCounts:
 
     def recordTweet(self, tweetName: str, time: int) -> None:
         bisect.insort(self.chunks[tweetName], time)
-        
+
     def getTweetCountsPerFrequency(self, freq: str, tweetName: str, startTime: int, endTime: int) -> List[int]:
-        period = 60
+        period = 60 #seconds
         if freq == "hour":
             period *= 60
         elif freq == "day":
             period *= (60 * 24)
         
-        
-        
-        res = [0] * ((endTime - startTime) // period + 1)
+        res = [0] * ((endTime - startTime) // period + 1 )
         
         start_idx = bisect.bisect_left(self.chunks[tweetName], startTime)
         end_idx = bisect.bisect_right(self.chunks[tweetName], endTime)
@@ -23,10 +21,11 @@ class TweetCounts:
         for i in range(start_idx, end_idx):
             ts = (self.chunks[tweetName][i] - startTime) // period
             res[ts] += 1
-        
+            
+            
         return res
-        
-        
+
+
 # Your TweetCounts object will be instantiated and called as such:
 # obj = TweetCounts()
 # obj.recordTweet(tweetName,time)
