43268f66f107c7cc3bcbca9ce90cd971d35122ab
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-08-23 07:11:04 -0700

    Time: 5082 ms (22.22%), Space: 73 MB (19.68%) - LeetHub

--- 1868-product-of-two-run-length-encoded-arrays/1868-product-of-two-run-length-encoded-arrays.py
+++ 1868-product-of-two-run-length-encoded-arrays/1868-product-of-two-run-length-encoded-arrays.py
@@ -1,32 +1,33 @@
 class Solution:
     def findRLEArray(self, encoded1: List[List[int]], encoded2: List[List[int]]) -> List[List[int]]:
-        
-        res = [] # num, freq
-        f1, f2 = 0, 0
-        v1, v2 = 0, 0
+        res = list()
         i, j = 0, 0
-        
         m, n = len(encoded1), len(encoded2)
+        v1, v2 = None, None
+        f1, f2 = None, None
         
-        while i < m and j < n:
+        while i < m or j < n:
+            
             if not f1 and i < m:
                 v1, f1 = encoded1[i]
+                
             if not f2 and j < n:
                 v2, f2 = encoded2[j]
-                
-            cur_min = min(f1, f2)
-            product = v1 * v2
+        
+            min_freq = min(f1, f2)
+            f1 -= min_freq
+            f2 -= min_freq
             
-            if res and res[-1][0] == product:
-                res[-1][1] += cur_min
+            curr_product = v1 * v2
+            if res and res[-1][0] == curr_product:
+                res[-1][1] += min_freq
             else:
-                res.append([product, cur_min])
-            
-            f1 -= cur_min
-            f2 -= cur_min
+                res.append([curr_product, min_freq])
             
             if not f1:
                 i += 1
+                
             if not f2:
                 j += 1
-        return res 
+        
+        return res
