d1660d947081e64a6fa20b8a3768a1306865c887
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-08-24 11:44:02 -0700

    Time: 184 ms (17.22%), Space: 14.1 MB (22.62%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.py
+++ 40-combination-sum-ii/40-combination-sum-ii.py
@@ -1,24 +1,25 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         candidates.sort()
-        res = list()
         
-        def backtrack(start, partial, curr):
+        def backtrack(start, curr, partial):
             if curr == target:
                 res.append(partial[:])
-            if curr >= target:
+                
+            if start >= len(candidates) or curr >= target:
                 return
             
-            prev = None
+            prev = -1
             for i in range(start, len(candidates)):
-                if prev and prev == candidates[i]:
+                if prev == candidates[i]:
                     continue
                     
                 partial.append(candidates[i])
-                backtrack(i + 1, partial, curr + candidates[i])
+                backtrack(i + 1, curr + candidates[i], partial)
                 partial.pop()
+                
                 prev = candidates[i]
         
-        backtrack(0, [], 0)
+        res = list()
+        backtrack(0, 0, [])
         return res
-            
