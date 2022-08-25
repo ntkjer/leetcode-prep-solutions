a3090fc905688c08414219404c8e6dfc714935fa
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-08-25 15:48:53 -0700

    Time: 85 ms (88.50%), Space: 15 MB (84.72%) - LeetHub

--- 174-dungeon-game/174-dungeon-game.py
+++ 174-dungeon-game/174-dungeon-game.py
@@ -1,30 +1,33 @@
 class Solution:
     def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
         m, n = len(dungeon), len(dungeon[-1])
+        dp = [[float('inf') for _ in range(n)] for _ in range(m)]
         
-        def is_valid_pos(r, c):
-            if not (0 <= r < m) or not(0 <= c < n): 
+        def isValidRow(r, c):
+            if not (0 <= r < m) or not(0 <= c < n):
                 return False
             return True
         
         
-        dp = [[float('inf') for _ in range(n)] for _ in range(m)]
-        
         for i in range(m - 1, -1, -1):
             for j in range(n - 1, -1, -1):
-                
-                up = float('inf')
-                left = float('inf')
-                
+                # check right, if we are in bounds
+                # check up, if we are in bound
                 curr = dungeon[i][j]
                 
-                if is_valid_pos(i + 1, j):
+                if isValidRow(i + 1, j):
                     up = max(dp[i + 1][j] - curr, 1)
-                if is_valid_pos(i, j + 1):
-                    left = max(dp[i][j + 1] - curr, 1)
+                else:
+                    up = float('inf')
+                
+                if isValidRow(i, j + 1):
+                    right = max(dp[i][j + 1] - curr, 1)
+                else:
+                    right = float('inf')
                 
                 
-                candidate = min(up, left)
+                candidate = min(up, right)
+                
                 if candidate != float('inf'):
                     dp[i][j] = candidate
                 else:
@@ -32,5 +35,7 @@ class Solution:
                         dp[i][j] = 1
                     else:
                         dp[i][j] = 1 - curr
+                
+        
         
         return dp[0][0]
