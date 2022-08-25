a2c3527696348dff23370b7b845d94bace29a109
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-08-25 16:08:24 -0700

    Time: 25 ms (99.24%), Space: 14 MB (32.00%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -1,22 +1,24 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if not digits: return ""
         
-        combos = {"0": set(), "1": set(), "2": set(["a", "b", "c"]), "3": set(["d", "e", "f"]),
-                 "4": set(["g", "h", "i"]), "5": set(["j", "k", "l"]), "6": set(["m", "n", "o"]),
-                 "7": set(["p", "q", "r", "s"]), "8": set(["t", "u", "v"]), "9": set(["w", "x", "y", "z"])}
+        res = list()
+        if not digits: return res
+        combinations = {"0": [], "1": [], "2": ["a", "b", "c"], "3": ["d", "e", "f"], 
+                        "4": ["g", "h", "i"], "5": ["j", "k", "l"], "6": ["m", "n", "o"], 
+                        "7": ["p", "q", "r", "s"], "8": ["t", "u", "v"], "9": ["w", "x", "y", "z"]}
         
-        def backtrack(start=0, partial=[]):
-            if start == len(digits):
-                res.append("".join(partial[:]))
-            if start >= len(digits):
+        
+        def backtrack(idx=0, partial=[]):
+            if idx == len(digits):
+                res.append("".join(partial[:]))    
+                
+            if idx >= len(digits): 
                 return
             
-            for letter in combos[digits[start]]:
-                partial.append(letter)
-                backtrack(start + 1)
+            for comb in combinations[digits[idx]]:
+                partial.append(comb)
+                backtrack(idx + 1, partial)
                 partial.pop()
-                
-        res = list()
+        
         backtrack()
         return res
