6859cb7e4e5df3b462188e2f468e472852a6e8dd
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-08-25 07:18:36 -0700

    Time: 54 ms (30.08%), Space: 13.7 MB (99.53%) - LeetHub

--- 468-validate-ip-address/468-validate-ip-address.py
+++ 468-validate-ip-address/468-validate-ip-address.py
@@ -1,33 +1,37 @@
 class Solution:
     def validIPAddress(self, queryIP: str) -> str:
-        
-        def validIPv4(ip):
-            ip = ip.split(".")
-            for x in ip:
-                if len(x) == 0 or len(x) > 3:
-                    return False
-                if x[0] == "0" and len(x) != 1 or not x.isdigit() or int(x) > 255:
-                    return False
-            return True
-        
-        def validIPv6(ip):
-            hexChars = "abcdefABCDEF0123456789"
-            hexSet = set([c for c in hexChars])
-            ip = ip.split(":")
-            
-            if len(ip) < 8:
-                return False
-            for block in ip:
+        def validateIPv4(ip):
+            data = ip.split(".")
+            for block in data:
                 if not (1 <= len(block) <= 4):
-                    return False
-                for c in block:
-                    if c not in hexSet:
-                        return False
-            return True
-        
-        if queryIP.count('.') == 3 and validIPv4(queryIP):
+                    return "Neither"
+                if len(block) > 1 and block[0] == "0":
+                    return "Neither"
+                if not(block.isdigit()):
+                    return "Neither"
+                if not(0 <= int(block) <= 255):
+                    return "Neither"
             return "IPv4"
-        elif queryIP.count(':') == 7 and validIPv6(queryIP):
+        
+        def validateIPv6(ip):
+            hexChars = "abcdefABCDEF0123456789"
+            hexCheck = set([char for char in hexChars])
+            
+            data = ip.split(":")
+            for block in data:
+                if not (1 <= len(block) <= 4): 
+                    return "Neither"
+                for x in block:
+                    if x not in hexCheck: 
+                        return "Neither"
+                    
             return "IPv6"
+        
+        if queryIP.count(".") == 3:
+            return validateIPv4(queryIP)
+        elif queryIP.count(":") == 7:
+            return validateIPv6(queryIP)
         else:
             return "Neither"
+        
+        
