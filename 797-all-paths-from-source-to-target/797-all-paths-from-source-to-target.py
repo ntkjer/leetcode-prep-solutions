377cf584c7bb729389f5445791b991808e543ce1
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-08-26 09:25:14 -0700

    Time: 181 ms (25.15%), Space: 15.7 MB (79.48%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
@@ -1,17 +1,16 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         
-        res = list()
-        
-        def backtrack(node, partial):
+        def backtrack(node, path):
             if node == len(graph) - 1:
-                res.append(partial[:])
+                res.append(path[:])
                 return
             
             for nei in graph[node]:
-                partial.append(nei)
-                backtrack(nei, partial)
-                partial.pop()
+                path.append(nei)
+                backtrack(nei, path)
+                path.pop()
                 
+        res = list()
         backtrack(0, [0])
         return res
