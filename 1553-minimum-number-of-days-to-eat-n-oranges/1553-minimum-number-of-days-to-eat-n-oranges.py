eb862fb4fd6d3837db2457f81489bd90f1499d53
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-08-25 17:22:51 -0700

    Time: 290 ms (13.33%), Space: 14.1 MB (84.52%) - LeetHub

--- 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.py
+++ 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.py
@@ -1,28 +1,28 @@
 class Solution:
     def minDays(self, n: int) -> int:
-        q = collections.deque()
-        
-        q.append(n)
+    
         days = 0
         seen = set()
+        
+        q = collections.deque()
+        q.append(n)
+        
         while q:
-            levels = len(q)
             days += 1
-            for _ in range(levels):
+                        
+            for _ in range(len(q)):
                 oranges = q.popleft()
-                
-                if oranges % 2 == 0 and oranges % 2 not in seen:
+                if not oranges - 1:
+                    return days
+                if (oranges - 1 > 0) and (oranges - 1) not in seen:
+                    q.append(oranges - 1)
+                    seen.add(oranges - 1)
+
+                if (oranges % 2 == 0) and (oranges % 2) not in seen:
                     q.append(oranges // 2)
                     seen.add(oranges // 2)
-                    
-                if oranges % 3 == 0 and oranges % 3 not in seen:
+                if (oranges % 3 == 0) and (oranges % 3) not in seen:
                     q.append(oranges // 3)
                     seen.add(oranges // 3)
-                    
-                if oranges - 1 not in seen:
-                    q.append(oranges - 1)
-                    
-                if oranges - 1 == 0:
-                    return days
-                
-        
+
+
