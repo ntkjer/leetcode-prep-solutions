ad6782d4e1936f72dd0bf31ea154b3a2a53196df
Niels Tam Kjer J.R <ntkjer@gmail.com>
2022-08-26 07:51:33 -0700

    Time: 62 ms (11.34%), Space: 13.8 MB (71.82%) - LeetHub

--- 394-decode-string/394-decode-string.py
+++ 394-decode-string/394-decode-string.py
@@ -1,26 +1,25 @@
 class Solution:
     def decodeString(self, s: str) -> str:
+        # 3 [ a 
+        # when a ] is encountered, evaluate the expression
+        # and push that eval'ed expr back onto the top of stack.
         
-        
-        # 3 [ a 2 [ c
-        # 3 [ a c c 
-        # accaccacc
         stack = []
+        
         for c in s:
-            if c == "]":
-                tmp = ""
-                while stack and stack[-1].isalpha():
-                    tmp = stack.pop() + tmp
-                
-                stack.pop()
+            if c == "]" and stack:
+                ch = ""
+                while stack and stack[-1] != "[":
+                    ch = stack.pop() + ch
+                    
+                stack.pop() # remove "["
                 freq = ""
                 while stack and stack[-1].isdigit():
                     freq = stack.pop() + freq
                 
-                stack.append(int(freq) * tmp)
+                stack.append(ch * int(freq))
                 
-                    
             else:
                 stack.append(c)
-                
+        
         return "".join(stack)
